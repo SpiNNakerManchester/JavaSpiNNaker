0d92cdf415c100e5342bf18532d0bfcef7620824
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2018-07-03 12:02:17 +0100

    Tuples

---
+++ SpiNNaker-machine/src/main/java/uk/ac/manchester/spinnaker/machine/datalinks/InetFpgaTuple.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2018 The University of Manchester
+ */
+package uk.ac.manchester.spinnaker.machine.datalinks;
+
+import java.net.InetAddress;
+import java.util.Objects;
+
+/**
+ *
+ * @author Christian-B
+ */
+public class InetFpgaTuple {
+
+    /** The InetAddress of this tuple which may be null. */
+    public final InetAddress address;
+
+    /** The id of fpga in this tuple. */
+    public final int fpga;
+
+    /** The id of link in this tuple. */
+    public final int linkId;
+
+    /**
+     *
+     * @param address The InetAddress of this tuple which may be null.
+     * @param fpga The id of fpga in this tuple.
+     * @param linkId  The id of link in this tuple.
+     */
+    public InetFpgaTuple(InetAddress address, int fpga, int linkId) {
+        this.address = address;
+        this.fpga = fpga;
+        this.linkId = linkId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 29 * hash + Objects.hashCode(this.address);
+        hash = 29 * hash + this.fpga;
+        hash = 29 * hash + this.linkId;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final InetFpgaTuple other = (InetFpgaTuple) obj;
+        if (this.fpga != other.fpga) {
+            return false;
+        }
+        if (this.linkId != other.linkId) {
+            return false;
+        }
+        if (!Objects.equals(this.address, other.address)) {
+            return false;
+        }
+        return true;
+    }
+
+
+
+}
