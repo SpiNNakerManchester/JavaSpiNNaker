3a76b504f6dede41321e9aa33a1f2e86ce66c426
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2018-07-21 09:50:03 +0100

    Improved memory reader. Memory flood writer.

--- SpiNNaker-comms/src/main/java/uk/ac/manchester/spinnaker/processes/ReadMemoryProcess.java
+++ SpiNNaker-comms/src/main/java/uk/ac/manchester/spinnaker/processes/ReadMemoryProcess.java
@@ -5,7 +5,9 @@ import static java.nio.ByteBuffer.allocate;
 import static java.nio.ByteOrder.LITTLE_ENDIAN;
 import static uk.ac.manchester.spinnaker.messages.Constants.UDP_MESSAGE_MAX_SIZE;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 
 import uk.ac.manchester.spinnaker.machine.HasChipLocation;
@@ -18,12 +20,16 @@ public class ReadMemoryProcess extends MultiConnectionProcess {
 		super(connectionSelector);
 	}
 
-	static class Accumulator {
+	private static class Accumulator {
 		private final ByteBuffer buffer;
 		private boolean done = false;
 
 		Accumulator(int size) {
 			buffer = allocate(size).order(LITTLE_ENDIAN);
+		}
+
+		Accumulator(ByteBuffer receivingBuffer) {
+			buffer = receivingBuffer.slice();
 		}
 
 		synchronized void add(int position, ByteBuffer otherBuffer) {
@@ -36,40 +42,172 @@ public class ReadMemoryProcess extends MultiConnectionProcess {
 			b.put(otherBuffer);
 		}
 
-		synchronized ByteBuffer get() {
+		synchronized ByteBuffer finish() {
 			done = true;
 			buffer.flip();
 			return buffer.asReadOnlyBuffer();
 		}
 	}
 
+	/**
+	 * This is complicated because the writes to the file can happen out of
+	 * order if the other end serves up the responses out of order. To handle
+	 * this, we need a seekable stream, and we need to make sure that we're not
+	 * stomping on our own toes when we do the seek.
+	 */
+	private static class FileAccumulator {
+		private final RandomAccessFile file;
+		private final long initOffset;
+		private boolean done = false;
+		private IOException exception;
+
+		FileAccumulator(RandomAccessFile dataStream) throws IOException {
+			file = dataStream;
+			initOffset = dataStream.getFilePointer();
+		}
+
+		synchronized void add(int position, ByteBuffer buffer) {
+			if (done) {
+				throw new IllegalStateException(
+						"writing to fully written buffer");
+			}
+			try {
+				file.seek(position + initOffset);
+				file.write(buffer.array(), buffer.position(),
+						buffer.remaining());
+			} catch (IOException e) {
+				if (exception == null) {
+					exception = e;
+				}
+			}
+		}
+
+		synchronized void finish() throws IOException {
+			done = true;
+			if (exception != null) {
+				throw exception;
+			}
+			file.seek(initOffset);
+		}
+	}
+
+	/** Read memory over a link into a prepared buffer. */
+	public void readLink(HasChipLocation chip, int linkID, int baseAddress,
+			ByteBuffer receivingBuffer) throws IOException, Exception {
+		int size = receivingBuffer.remaining();
+		Accumulator a = new Accumulator(receivingBuffer);
+		int chunk;
+		for (int offset = 0; offset < size; offset += chunk) {
+			chunk = min(size, UDP_MESSAGE_MAX_SIZE);
+			int thisOffset = offset;
+			sendRequest(new ReadLink(chip, linkID, baseAddress + offset, chunk),
+					response -> a.add(thisOffset, response.data));
+		}
+		finish();
+		checkForError();
+		a.finish();
+	}
+
+	/** Read memory into a prepared buffer. */
+	public void readMemory(HasChipLocation chip, int baseAddress,
+			ByteBuffer receivingBuffer) throws IOException, Exception {
+		int size = receivingBuffer.remaining();
+		Accumulator a = new Accumulator(receivingBuffer);
+		int chunk;
+		for (int offset = 0; offset < size; offset += chunk) {
+			chunk = min(size, UDP_MESSAGE_MAX_SIZE);
+			int thisOffset = offset;
+			sendRequest(new ReadMemory(chip, baseAddress + offset, chunk),
+					response -> a.add(thisOffset, response.data));
+		}
+		finish();
+		checkForError();
+		a.finish();
+	}
+
+	/** Read memory over a link into a new buffer. */
 	public ByteBuffer readLink(HasChipLocation chip, int linkID,
 			int baseAddress, int size) throws IOException, Exception {
 		Accumulator a = new Accumulator(size);
 		int chunk;
 		for (int offset = 0; offset < size; offset += chunk) {
 			chunk = min(size, UDP_MESSAGE_MAX_SIZE);
-			int bufferPosition = offset;
+			int thisOffset = offset;
 			sendRequest(new ReadLink(chip, linkID, baseAddress + offset, chunk),
-					response -> a.add(bufferPosition, response.data));
+					response -> a.add(thisOffset, response.data));
 		}
 		finish();
 		checkForError();
-		return a.get();
+		return a.finish();
 	}
 
+	/** Read memory into a new buffer. */
 	public ByteBuffer readMemory(HasChipLocation chip, int baseAddress,
 			int size) throws IOException, Exception {
 		Accumulator a = new Accumulator(size);
 		int chunk;
 		for (int offset = 0; offset < size; offset += chunk) {
 			chunk = min(size, UDP_MESSAGE_MAX_SIZE);
-			int bufferPosition = offset;
+			int thisOffset = offset;
 			sendRequest(new ReadMemory(chip, baseAddress + offset, chunk),
-					response -> a.add(bufferPosition, response.data));
+					response -> a.add(thisOffset, response.data));
 		}
 		finish();
 		checkForError();
-		return a.get();
+		return a.finish();
+	}
+
+	/**
+	 * Read memory over a link into a file. Note that we can write the file out
+	 * of order; a {@link RandomAccessFile} is required
+	 */
+	public void readLink(HasChipLocation chip, int linkID, int baseAddress,
+			int size, RandomAccessFile dataFile) throws IOException, Exception {
+		FileAccumulator a = new FileAccumulator(dataFile);
+		int chunk;
+		for (int offset = 0; offset < size; offset += chunk) {
+			chunk = min(size, UDP_MESSAGE_MAX_SIZE);
+			int thisOffset = offset;
+			sendRequest(new ReadLink(chip, linkID, baseAddress + offset, chunk),
+					response -> a.add(thisOffset, response.data));
+		}
+		finish();
+		checkForError();
+		a.finish();
+	}
+
+	/**
+	 * Read memory into a file. Note that we can write the file out of order; a
+	 * {@link RandomAccessFile} is required
+	 */
+	public void readMemory(HasChipLocation chip, int baseAddress, int size,
+			RandomAccessFile dataFile) throws IOException, Exception {
+		FileAccumulator a = new FileAccumulator(dataFile);
+		int chunk;
+		for (int offset = 0; offset < size; offset += chunk) {
+			chunk = min(size, UDP_MESSAGE_MAX_SIZE);
+			int thisOffset = offset;
+			sendRequest(new ReadMemory(chip, baseAddress + offset, chunk),
+					response -> a.add(thisOffset, response.data));
+		}
+		finish();
+		checkForError();
+		a.finish();
+	}
+
+	/** Read memory over a link into a file. */
+	public void readLink(HasChipLocation chip, int linkID, int baseAddress,
+			int size, File dataFile) throws IOException, Exception {
+		try (RandomAccessFile s = new RandomAccessFile(dataFile, "rw")) {
+			readLink(chip, linkID, baseAddress, size, s);
+		}
+	}
+
+	/** Read memory into a file. */
+	public void readMemory(HasChipLocation chip, int baseAddress, int size,
+			File dataFile) throws IOException, Exception {
+		try (RandomAccessFile s = new RandomAccessFile(dataFile, "rw")) {
+			readMemory(chip, baseAddress, size, s);
+		}
 	}
 }
---
+++ SpiNNaker-comms/src/main/java/uk/ac/manchester/spinnaker/processes/WriteMemoryFloodProcess.java
@@ -0,0 +1,131 @@
+package uk.ac.manchester.spinnaker.processes;
+
+import static java.lang.Math.ceil;
+import static java.lang.Math.min;
+import static uk.ac.manchester.spinnaker.messages.Constants.UDP_MESSAGE_MAX_SIZE;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
+import uk.ac.manchester.spinnaker.messages.scp.FloodFillData;
+import uk.ac.manchester.spinnaker.messages.scp.FloodFillEnd;
+import uk.ac.manchester.spinnaker.messages.scp.FloodFillStart;
+
+/** A process for writing memory on multiple SpiNNaker chips at once. */
+public class WriteMemoryFloodProcess extends MultiConnectionProcess {
+	public WriteMemoryFloodProcess(ConnectionSelector connectionSelector) {
+		super(connectionSelector);
+	}
+
+	public WriteMemoryFloodProcess(ConnectionSelector connectionSelector,
+			int numRetries, int timeout, int numChannels,
+			int intermediateChannelWaits) {
+		super(connectionSelector, numRetries, timeout, numChannels,
+				intermediateChannelWaits);
+	}
+
+	private static int numBlocks(int numBytes) {
+		return (int) ceil(ceil(numBytes / 4.0) / UDP_MESSAGE_MAX_SIZE);
+	}
+
+	/**
+	 * Flood fills memory with data from a buffer.
+	 *
+	 * @param nearestNeighbourID
+	 *            The ID of the fill
+	 * @param baseAddress
+	 *            Where the data is to be written to
+	 * @param data
+	 *            The data, from the <i>position</i> (inclusive) to the
+	 *            <i>limit</i> (exclusive)
+	 */
+	public void writeMemory(int nearestNeighbourID, int baseAddress,
+			ByteBuffer data) throws IOException, Exception {
+		data = data.asReadOnlyBuffer();
+		int numBytes = data.remaining();
+		synchronousCall(
+				new FloodFillStart(nearestNeighbourID, numBlocks(numBytes)));
+
+		int blockID = 0;
+		while (numBytes > 0) {
+			int chunk = min(numBytes, UDP_MESSAGE_MAX_SIZE);
+			ByteBuffer tmp = data.duplicate();
+			tmp.limit(tmp.position() + chunk);
+			sendRequest(new FloodFillData(nearestNeighbourID, blockID,
+					baseAddress, tmp));
+			blockID++;
+			numBytes -= chunk;
+			baseAddress += chunk;
+			data.position(data.position() + chunk);
+		}
+		finish();
+		checkForError();
+
+		synchronousCall(new FloodFillEnd(nearestNeighbourID));
+	}
+
+	/**
+	 * Flood fills memory with data from an input stream.
+	 *
+	 * @param nearestNeighbourID
+	 *            The ID of the fill
+	 * @param baseAddress
+	 *            Where the data is to be written to
+	 * @param dataStream
+	 *            The place to get the data from.
+	 * @param numBytes
+	 *            The number of bytes to read. Be aware that you can specify a
+	 *            number larger than the number of bytes actually available; if
+	 *            you do so, the fill will terminate early and this may cause
+	 *            problems.
+	 */
+	public void writeMemory(int nearestNeighbourID, int baseAddress,
+			InputStream dataStream, int numBytes)
+			throws IOException, Exception {
+		synchronousCall(
+				new FloodFillStart(nearestNeighbourID, numBlocks(numBytes)));
+
+		int blockID = 0;
+		while (numBytes > 0) {
+			int chunk = min(numBytes, UDP_MESSAGE_MAX_SIZE);
+			// Allocate a new array each time; assume message hold a ref to it
+			byte[] buffer = new byte[chunk];
+			chunk = dataStream.read(buffer);
+			if (chunk <= 0) {
+				break;
+			}
+			sendRequest(new FloodFillData(nearestNeighbourID, blockID,
+					baseAddress, buffer, 0, chunk));
+			blockID++;
+			numBytes -= chunk;
+			baseAddress += chunk;
+		}
+		finish();
+		checkForError();
+
+		synchronousCall(new FloodFillEnd(nearestNeighbourID));
+	}
+
+	/**
+	 * Flood fills memory with data from a file.
+	 *
+	 * @param nearestNeighbourID
+	 *            The ID of the fill
+	 * @param baseAddress
+	 *            Where the data is to be written to
+	 * @param dataFile
+	 *            The data in a file, which will be fully transferred.
+	 */
+	public void writeMemory(int nearestNeighbourID, int baseAddress,
+			File dataFile) throws IOException, Exception {
+		try (InputStream s = new BufferedInputStream(
+				new FileInputStream(dataFile))) {
+			writeMemory(nearestNeighbourID, baseAddress, s,
+					(int) dataFile.length());
+		}
+	}
+}
