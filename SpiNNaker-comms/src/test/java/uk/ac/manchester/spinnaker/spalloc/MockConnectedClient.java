61a88c80dabb7aeea44fbda26bfd3ec68bba1ced
micro <christian.brenninkmeijer@manchester.ac.uk>
2018-09-19 09:17:42 +0100

    More testing of json

--- SpiNNaker-comms/src/test/java/uk/ac/manchester/spinnaker/spalloc/MockConnectedClient.java
+++ SpiNNaker-comms/src/test/java/uk/ac/manchester/spinnaker/spalloc/MockConnectedClient.java
@@ -2,7 +2,15 @@ package uk.ac.manchester.spinnaker.spalloc;
 
 import java.io.IOException;
 import uk.ac.manchester.spinnaker.spalloc.messages.Command;
+import uk.ac.manchester.spinnaker.spalloc.messages.CreateJobCommand;
+import uk.ac.manchester.spinnaker.spalloc.messages.GetJobMachineInfoCommand;
+import uk.ac.manchester.spinnaker.spalloc.messages.GetJobStateCommand;
 import uk.ac.manchester.spinnaker.spalloc.messages.ListJobsCommand;
+import uk.ac.manchester.spinnaker.spalloc.messages.ListMachinesCommand;
+import uk.ac.manchester.spinnaker.spalloc.messages.PowerOffJobBoardsCommand;
+import uk.ac.manchester.spinnaker.spalloc.messages.PowerOnJobBoardsCommand;
+import uk.ac.manchester.spinnaker.spalloc.messages.WhereIsJobChipCommand;
+import uk.ac.manchester.spinnaker.spalloc.messages.WhereIsMachineChipCommand;
 
 /**
  *
@@ -10,7 +18,6 @@ import uk.ac.manchester.spinnaker.spalloc.messages.ListJobsCommand;
  */
 public class MockConnectedClient extends SpallocClient {
 
-    static final String LIST_JOBS_C = (new ListJobsCommand()).getCommand();
     static final String LIST_JOBS_R = "["
             + "{\"job_id\":47224,"
                 + "\"owner\":\"someone@manchester.ac.uk\","
@@ -46,18 +53,99 @@ public class MockConnectedClient extends SpallocClient {
                 + "\"keepalivehost\":\"130.88.198.171\"}]";
 
     
+    static final String LIST_MACHINE_R = "["
+        + "{\"name\":\"Spin24b-223\","
+            + "\"tags\":[\"default\",\"machine-room\"],"
+            + "\"width\":4,"
+            + "\"height\":2,"
+            + "\"dead_boards\":[],"
+            + "\"dead_links\":[]},"
+        + "{\"name\":\"Spin24b-225\","
+            + "\"tags\":[\"default\",\"machine-room\"],"
+            + "\"width\":4,"
+            + "\"height\":2,"
+            + "\"dead_boards\":[],"
+            + "\"dead_links\":[]},"
+        + "{\"name\":\"Spin24b-226\","
+            + "\"tags\":[\"default\","
+            + "\"machine-room\"],"
+            + "\"width\":4,"
+            + "\"height\":2,"
+            + "\"dead_boards\":[],"
+            + "\"dead_links\":[]}"
+        + "]";
     
-    public MockConnectedClient() {
-        super("127.0.0.0", 8080, 2000);
+    static final String JOB_MACHINE_INFO_R = 
+            "{\"connections\":[[[0,0],\"10.11.223.33\"]],"
+            + "\"width\":8,"
+            + "\"machine_name\":\"Spin24b-223\","
+            + "\"boards\":[[0,0,2]],"
+            + "\"height\":8}";
+    
+    static final String STATE_POWER_R = 
+            "{\"state\":2,"
+            + "\"power\":true,"
+            + "\"keepalive\":60.0,"
+            + "\"reason\":null,"
+            + "\"start_time\":1.537284307847865E9,"
+            + "\"keepalivehost\":\"86.82.216.229\"}";
+            
+    
+    static final String WHERE_IS_CHIP = 
+            "{\"job_chip\":[1,1],"
+            + "\"job_id\":47894,"
+            + "\"chip\":[5,9],"
+            + "\"logical\":[0,0,2],"
+            + "\"machine\":\"Spin24b-223\","
+            + "\"board_chip\":[1,1],"
+            + "\"physical\":[0,0,4]}";
+            
+    private boolean actual;
+ 
+    public MockConnectedClient(int timeout) {
+        //super("spinnaker.cs.man.ac.uk", 22244, timeout);
+        super("127.0.0.0", 22244, timeout);
+        actual = true;
     }
     
     public void connect(Integer timeout) throws IOException {
-        //DO nothing
+        if (actual) {
+            try {
+                super.connect(timeout);
+                actual = true;
+            } catch (Exception ex) {
+                actual = false;
+                System.out.println("Connect fail using mock");
+            }
+        }
     }
 
     protected String call(Command<?> command, Integer timeout) {
+        if (actual) {
+            try {
+                return super.call(command, timeout);
+            } catch (Exception ex) {
+                actual = false;
+                System.out.println("Call fail using mock");
+            }
+        }
         if (command.getClass() == ListJobsCommand.class) {
             return LIST_JOBS_R;
+        }
+        if (command.getClass() == ListMachinesCommand.class) {
+            return LIST_MACHINE_R;
+        }
+        if (command.getClass() == CreateJobCommand.class) {
+            return "99999";
+        }
+        if (command.getClass() == GetJobMachineInfoCommand.class) {
+            return JOB_MACHINE_INFO_R;
+        }        
+        if (command.getClass() ==GetJobStateCommand.class) {
+            return STATE_POWER_R;
+        }
+        if (command.getClass() == WhereIsJobChipCommand.class) {
+            return WHERE_IS_CHIP;
         }
         return null;
     }
--- SpiNNaker-comms/src/test/java/uk/ac/manchester/spinnaker/spalloc/TestMockClient.java
+++ SpiNNaker-comms/src/test/java/uk/ac/manchester/spinnaker/spalloc/TestMockClient.java
@@ -1,10 +1,19 @@
 package uk.ac.manchester.spinnaker.spalloc;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
+import uk.ac.manchester.spinnaker.machine.ChipLocation;
 import uk.ac.manchester.spinnaker.spalloc.exceptions.SpallocServerException;
 import uk.ac.manchester.spinnaker.spalloc.messages.JobDescription;
+import uk.ac.manchester.spinnaker.spalloc.messages.JobMachineInfo;
+import uk.ac.manchester.spinnaker.spalloc.messages.JobState;
+import uk.ac.manchester.spinnaker.spalloc.messages.Machine;
+import uk.ac.manchester.spinnaker.spalloc.messages.WhereIs;
 
 /**
  *
@@ -12,15 +21,44 @@ import uk.ac.manchester.spinnaker.spalloc.messages.JobDescription;
  */
 public class TestMockClient {
 
+        int timeout = 1000;
+        SpallocClient client = new MockConnectedClient(timeout);
+        
 	@Test
-        void testlistJobs() throws IOException, SpallocServerException, Exception {
-            SpallocClient client = new MockConnectedClient();
+        void testListJobs() throws IOException, SpallocServerException, Exception {
             try (AutoCloseable c = client.withConnection()) {
-                List<JobDescription> jobs = client.listJobs();
-                for (JobDescription job:jobs) {
-                    System.out.println(job);
-                }
+                List<JobDescription> jobs = client.listJobs(timeout);
+                assertNotNull(jobs);
             }
 
+        }
+
+        @Test
+        void testListMachines() throws IOException, SpallocServerException, Exception {
+            try (AutoCloseable c = client.withConnection()) {
+                List<Machine> machines = client.listMachines(timeout);
+                assertNotNull(machines);
+            }
+        }
+        
+        @Test
+        void testJob() throws IOException, SpallocServerException, Exception {
+            try (AutoCloseable c = client.withConnection()) {
+                List<Integer> args = new ArrayList<>();
+                Map<String, Object> kwargs = new HashMap<>();   
+                kwargs.put("owner", "Unittest. OK to kill after 1 minute.");
+                int jobId = client.createJob(args, kwargs, timeout);
+                System.out.println(jobId);
+                JobMachineInfo machineInfo = client.getJobMachineInfo(jobId, timeout);
+                System.out.println(machineInfo);
+                JobState state = client.getJobState(jobId, timeout);
+                System.out.println(state.getState());
+                client.jobKeepAlive(jobId, timeout);
+                client.powerOffJobBoards(jobId, timeout);
+                client.powerOnJobBoards(jobId, timeout);
+                WhereIs whereis = client.whereIs(jobId, new ChipLocation(1,1), timeout);
+                System.out.println(whereis);
+                client.destroyJob(jobId, "Test finished", timeout);
+             }   
         }
 }
