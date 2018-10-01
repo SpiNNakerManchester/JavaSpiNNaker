21296b05f15a6410710b400cdcfc86780ccf5f68
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2018-10-01 14:01:57 +0100

    First cut at vaguely useful storage system.
    Still *very* inefficient!

--- SpiNNaker-storage/src/main/java/uk/ac/manchester/spinnaker/storage/DatabaseEngine.java
+++ SpiNNaker-storage/src/main/java/uk/ac/manchester/spinnaker/storage/DatabaseEngine.java
@@ -1,6 +1,7 @@
 package uk.ac.manchester.spinnaker.storage;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
 import static org.apache.commons.io.IOUtils.resourceToString;
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -8,10 +9,14 @@ import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 import org.slf4j.Logger;
+
+import uk.ac.manchester.spinnaker.machine.HasCoreLocation;
 
 /**
  * The database engine interface. Based on SQLite.
@@ -30,17 +35,27 @@ public class DatabaseEngine {
 		}
 	}
 
+	private File dbFile;
+
 	/**
-	 * Get a connection to an SQLite database, creating it if needed.
+	 * Create an engine interface for a particular database.
 	 *
 	 * @param dbFile
 	 *            The file containing the database.
+	 */
+	public DatabaseEngine(File dbFile) {
+		this.dbFile = dbFile;
+	}
+
+	/**
+	 * Get a connection to an SQLite database, creating it if needed.
+	 *
 	 * @return The configured connection to the database. The database will have
 	 *         been seeded with DDL if necessary.
 	 * @throws SQLException
 	 *             If anything goes wrong.
 	 */
-	Connection getConnection(File dbFile) throws SQLException {
+	Connection getConnection() throws SQLException {
 		String url = "jdbc:sqlite:" + dbFile.getAbsolutePath();
 		log.debug("opening database connection {}", url);
 		Connection conn = DriverManager.getConnection(url);
@@ -48,5 +63,61 @@ public class DatabaseEngine {
 			statement.executeUpdate(sqlDDL);
 		}
 		return conn;
+	}
+
+	private static final String STORE =
+			"INSERT INTO storage(x, y, processor, region, content) "
+					+ "VALUES(?, ?, ?, ?, ?)";
+	private static final String FETCH = "SELECT content FROM storage WHERE "
+			+ "x = ? AND y = ? AND processor = ? AND region = ?";
+	private static final String DELETE = "DELETE FROM storage WHERE "
+			+ "x = ? AND y = ? AND processor = ? AND region = ?";
+
+	public int storeRegionContents(HasCoreLocation core, int region,
+			byte[] contents) throws SQLException {
+		try (Connection conn = getConnection();
+				PreparedStatement s =
+						conn.prepareStatement(STORE, RETURN_GENERATED_KEYS)) {
+			s.setInt(1, core.getX());
+			s.setInt(2, core.getY());
+			s.setInt(3, core.getP());
+			s.setInt(4, region);
+			s.setBytes(5, contents);
+			s.executeUpdate();
+			try (ResultSet keys = s.getGeneratedKeys()) {
+				keys.next();
+				return keys.getInt(1);
+			}
+		}
+	}
+
+	public byte[] getRegionContents(HasCoreLocation core, int region)
+			throws SQLException {
+		try (Connection conn = getConnection();
+				PreparedStatement s = conn.prepareStatement(FETCH)) {
+			s.setInt(1, core.getX());
+			s.setInt(2, core.getY());
+			s.setInt(3, core.getP());
+			s.setInt(4, region);
+			try (ResultSet rs = s.executeQuery()) {
+				while (rs.next()) {
+					return rs.getBytes(1);
+				}
+				throw new IllegalArgumentException(
+						"core " + core + " has no data for region " + region);
+			}
+		}
+	}
+
+	public void deleteRegionContents(HasCoreLocation core, int region)
+			throws SQLException {
+		try (Connection conn = getConnection();
+				PreparedStatement s = conn.prepareStatement(DELETE)) {
+			s.setInt(1, core.getX());
+			s.setInt(2, core.getY());
+			s.setInt(3, core.getP());
+			s.setInt(4, region);
+			s.executeUpdate();
+		}
 	}
 }
--- SpiNNaker-storage/src/main/resources/db.sql
+++ SpiNNaker-storage/src/main/resources/db.sql
@@ -5,5 +5,8 @@ PRAGMA foreign_keys = ON;
 -- A table mapping unique names to blobs of data. It's trivial!
 CREATE TABLE IF NOT EXISTS storage(
 	storage_id INTEGER PRIMARY KEY AUTOINCREMENT,
-	name TEXT UNIQUE ON CONFLICT FAIL,
+	x INTEGER NOT NULL,
+	y INTEGER NOT NULL,
+	processor INTEGER NOT NULL,
+	region INTEGER NOT NULL,
 	content BLOB);
