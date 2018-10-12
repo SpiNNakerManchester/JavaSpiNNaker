1bd2e9bc802a267f0860589ae34cdbc76a068fec
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2018-10-12 11:38:52 +0100

    Better ping options for testing

--- SpiNNaker-utils/src/test/java/uk/ac/manchester/spinnaker/utils/TestPing.java
+++ SpiNNaker-utils/src/test/java/uk/ac/manchester/spinnaker/utils/TestPing.java
@@ -13,14 +13,30 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author Christian-B
  */
 public class TestPing {
-
-    public TestPing() {
+    @Test
+    public void testPingSpalloc() throws UnknownHostException {
+        InetAddress spalloc = InetAddress.getByName("spinnaker.cs.man.ac.uk");
+        // Should be able to reach Spalloc...
+        assertEquals(0, Ping.ping(spalloc));
     }
 
     @Test
-    public void testTravis() throws UnknownHostException {
-        InetAddress travis = InetAddress.getByName("travis-ci.org");
-        // *REALLY* should be able to reach Travis...
+    public void testPingGoogle() throws UnknownHostException {
+        InetAddress travis = InetAddress.getByName("8.8.8.8");
+        // *REALLY* should be able to reach Google's DNS...
         assertEquals(0, Ping.ping(travis));
+    }
+
+    @Test
+    public void testPingLocalhost() throws UnknownHostException {
+    	// Can't ping localhost? Network catastrophically bad!
+        assertEquals(0, Ping.ping("localhost"));
+    }
+
+    @Test
+    public void testPingDownHost() throws UnknownHostException {
+    	// Definitely unpingable host
+    	// http://answers.microsoft.com/en-us/windows/forum/windows_vista-networking/invalid-ip-address-169254xx/ce096728-e2b7-4d54-80cc-52a4ed342870
+        assertNotEquals(0, Ping.ping("169.254.254.254"));
     }
 }
