c7ca02acdefd8dcbcbf01c75ab4e6afbbf886f8e
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2018-10-26 14:19:04 +0100

    checkstyle

--- SpiNNaker-comms/src/main/java/uk/ac/manchester/spinnaker/messages/boot/BootDataBlock.java
+++ SpiNNaker-comms/src/main/java/uk/ac/manchester/spinnaker/messages/boot/BootDataBlock.java
@@ -11,16 +11,20 @@ import java.nio.ByteBuffer;
  * @author Donal Fellows
  */
 class BootDataBlock extends BootMessage {
-	private static final int BOOT_DATA_OPERAND_1 =
-			(BOOT_MESSAGE_DATA_WORDS - 1) << 8;
 	private static final int BYTE_MASK = 0xFF;
+	private static final int BYTE_SIZE = 8;
+	private static final int BOOT_DATA_OPERAND_1 =
+			(BOOT_MESSAGE_DATA_WORDS - 1) << BYTE_SIZE;
+
+	private static final boolean inByteRange(int value) {
+		return (value & BYTE_MASK) == value;
+	}
 
 	BootDataBlock(int blockID, ByteBuffer buffer) {
 		super(FLOOD_FILL_BLOCK, BOOT_DATA_OPERAND_1 | blockID, 0, 0, buffer);
 
 		// Sanity checks
-		assert (blockID
-				& BYTE_MASK) == blockID : "block ID must fit in byte range";
+		assert inByteRange(blockID) : "block ID must fit in byte range";
 		assert buffer.hasRemaining() : "buffer must have space left";
 	}
 }
