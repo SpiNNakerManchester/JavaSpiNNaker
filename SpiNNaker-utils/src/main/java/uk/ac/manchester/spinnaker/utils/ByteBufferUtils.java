3b959263a7b590980d7a3ecf2e702d05f72fddd5
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2018-10-30 11:16:07 +0000

    debug methods for ByteBuffers

---
+++ SpiNNaker-utils/src/main/java/uk/ac/manchester/spinnaker/utils/ByteBufferUtils.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2018 The University of Manchester
+ */
+package uk.ac.manchester.spinnaker.utils;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+/**
+ *
+ * @author Christian-B
+ */
+public class ByteBufferUtils {
+
+
+    public static String asString(ByteBuffer bb) {
+        return asString(bb, 0, bb.position());
+    }
+
+    public static String asString(ByteBuffer bb, int from) {
+        return asString(bb, from, bb.position());
+    }
+
+    public static String asString(ByteBuffer bb, int from, int to) {
+        return "(" + bb.position() + ")" + asHex(bb, from, to);
+    }
+
+    public static String byteArrayToHex(ByteBuffer bb) {
+        return asHex(bb, 0, bb.position());
+    }
+
+    public static String byteArrayToHex(ByteBuffer bb, int start) {
+        return asHex(bb, 0, start);
+    }
+
+    public static String asHex(ByteBuffer bb, int from, int to) {
+        return asHex(Arrays.copyOfRange(bb.array(), from, to));
+    }
+
+    public static String asHex(byte[] ba) {
+        StringBuilder sb = new StringBuilder();
+        for(byte b: ba)
+            sb.append(String.format("%02x,", b));
+        return sb.toString();
+    }
+
+}
