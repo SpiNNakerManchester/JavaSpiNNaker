deb049b6bfaea5f9f1ed59e3d5eacb2b58113d3d
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2018-11-23 14:57:55 +0000

    Basic per-file copyright notices: comms

--- SpiNNaker-comms/src/test/java/uk/ac/manchester/spinnaker/spalloc/messages/TestBoardCoordinates.java
+++ SpiNNaker-comms/src/test/java/uk/ac/manchester/spinnaker/spalloc/messages/TestBoardCoordinates.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2018 The University of Manchester
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package uk.ac.manchester.spinnaker.spalloc.messages;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,7 +27,7 @@ import uk.ac.manchester.spinnaker.spalloc.SpallocClient;
  * @author Christian
  */
 public class TestBoardCoordinates {
-    
+
     @Test
     void testFromJson() throws IOException {
         String json = "[2, 4, 6]";
@@ -20,11 +36,11 @@ public class TestBoardCoordinates {
         assertEquals(2, fromJson.getX());
         assertEquals(4, fromJson.getY());
         assertEquals(6, fromJson.getZ());
-        
+
         BoardCoordinates direct = new BoardCoordinates(2, 4, 6);
         assertEquals(direct, fromJson);
         assertEquals(direct.hashCode(), fromJson.hashCode());
         assertEquals(direct.toString(), fromJson.toString());
     }
-    
+
 }
