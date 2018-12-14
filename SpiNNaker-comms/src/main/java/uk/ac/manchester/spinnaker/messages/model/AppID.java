745f82eadd47a90272db2e293f5bc797c23633ca
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2018-12-14 11:15:43 +0000

    Rats! Checkstyle!

--- SpiNNaker-comms/src/main/java/uk/ac/manchester/spinnaker/messages/model/AppID.java
+++ SpiNNaker-comms/src/main/java/uk/ac/manchester/spinnaker/messages/model/AppID.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2018 The University of Manchester
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package uk.ac.manchester.spinnaker.messages.model;
 
 /**
@@ -13,7 +29,7 @@ public final class AppID {
 	private static final int MAX_APP_ID = 255;
 
 	/**
-	 * Th default application ID, used when code doesn't have a better idea.
+	 * The default application ID, used when code doesn't have a better idea.
 	 */
 	public static final AppID DEFAULT = new AppID(0);
 
@@ -22,6 +38,12 @@ public final class AppID {
 	 */
 	public final int appID;
 
+	/**
+	 * Create an application ID.
+	 *
+	 * @param appID
+	 *            The proposed ID of the application. Must be between 0 and 255.
+	 */
 	public AppID(int appID) {
 		if (appID < 0 || appID > MAX_APP_ID) {
 			throw new IllegalArgumentException(
