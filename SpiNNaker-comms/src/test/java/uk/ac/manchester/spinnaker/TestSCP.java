309e7e67d37d40d22d0534ad2bcbbc1b0c4a2c57
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2019-11-05 09:59:40 +0000

    Added more test

--- SpiNNaker-comms/src/test/java/uk/ac/manchester/spinnaker/TestSCP.java
+++ SpiNNaker-comms/src/test/java/uk/ac/manchester/spinnaker/TestSCP.java
@@ -1,17 +1,23 @@
 package uk.ac.manchester.spinnaker;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import uk.ac.manchester.spinnaker.connections.SCPConnection;
+import uk.ac.manchester.spinnaker.connections.SDPConnection;
 import uk.ac.manchester.spinnaker.connections.selectors.ConnectionSelector;
 import uk.ac.manchester.spinnaker.machine.ChipLocation;
 import uk.ac.manchester.spinnaker.machine.CoreSubsets;
@@ -26,7 +32,6 @@ import uk.ac.manchester.spinnaker.messages.scp.SCPRequest;
 import uk.ac.manchester.spinnaker.messages.sdp.SDPHeader;
 import uk.ac.manchester.spinnaker.messages.sdp.SDPHeader.Flag;
 import uk.ac.manchester.spinnaker.spalloc.SpallocJob;
-import uk.ac.manchester.spinnaker.transceiver.RetryTracker;
 import uk.ac.manchester.spinnaker.transceiver.Transceiver;
 import uk.ac.manchester.spinnaker.transceiver.processes.MultiConnectionProcess;
 import uk.ac.manchester.spinnaker.transceiver.processes.ProcessException;
@@ -35,8 +40,7 @@ import uk.ac.manchester.spinnaker.utils.progress.ProgressBar;
 public class TestSCP {
 
     public static void main(String[] args) throws Exception {
-        URL execUri = ClassLoader.getSystemResource("scp_test.aplx");
-        File executable = new File(execUri.getFile());
+        File executable = new File("src/test/resources/scp_test.aplx");
         if (!executable.exists()) {
             throw new FileNotFoundException(executable.getAbsolutePath());
         }
@@ -63,10 +67,85 @@ public class TestSCP {
                 System.err.println("Running SCP test");
                 TestProcess process = new TestProcess(txrx.getScampConnectionSelector());
                 process.test(coreSubsets, 1000, 3);
+
+                System.err.println("Sending Data");
+                SDPConnection conn = new SDPConnection(
+                        new ChipLocation(0, 0), hostname, 17894);
+                byte[][] inputData = new byte[1000][];
+                Receiver receiver = new Receiver(1000, conn);
+                receiver.start();
+                for (int i = 0; i < 1000; i++) {
+                    ByteArrayOutputStream bytes = new ByteArrayOutputStream(1464);
+                    DataOutputStream data = new DataOutputStream(bytes);
+                    data.writeInt(i);
+                    Random r = new Random();
+                    for (int j = 0; j < 1460; j++) {
+                       data.writeByte(r.nextInt(255) - 128);
+                    }
+                    inputData[i] = bytes.toByteArray();
+                    conn.send(inputData[i]);
+                }
+
+                System.err.println("Waiting for receive to finish");
+                while (!receiver.isError()) {
+                    Thread.sleep(500);
+                }
+
+                int lastReceived = -1;
+                for (int i = 0; i < inputData.length; i++) {
+                    if (receiver.received[i] != null) {
+                        if (lastReceived + 1 != i) {
+                            System.err.println("Missing " + lastReceived + " - " + (i - 1));
+                        }
+                        lastReceived = i;
+                        System.err.println("Received " + i);
+                        if (!Arrays.equals(inputData[i], receiver.received[i])) {
+                            System.err.println("    Not equal!");
+                            System.err.println("    " + inputData[i]);
+                            System.err.println("    " + receiver.received[i]);
+                        }
+                    }
+                }
+
+                System.err.println(txrx.getScampVersion());
+                System.err.println(txrx.getScampVersion(new ChipLocation(1, 1)));
             }
         }
     }
 
+}
+
+
+final class Receiver extends Thread {
+    public final byte[][] received;
+
+    private final SDPConnection connection;
+
+    private boolean error = false;
+
+    public Receiver(int size, SDPConnection connection) {
+        received = new byte[size][];
+        this.connection = connection;
+    }
+
+    public void run() {
+        while (!error) {
+            try {
+                ByteBuffer data = connection.receive(2000);
+                int index = data.getInt();
+                received[index] = new byte[data.capacity()];
+                data.rewind();
+                data.get(received[index]);
+            } catch (Exception e) {
+                e.printStackTrace();
+                error = true;
+            }
+        }
+    }
+
+    public boolean isError() {
+        return error;
+    }
 }
 
 
