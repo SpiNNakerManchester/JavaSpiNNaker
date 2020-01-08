b6c2412ebe397cfc9c5f6c402bae8c356deaf31d
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2020-01-08 14:24:07 +0000

    Update to a version of log4j without a security advisory against it
    Of course, that version doesn't configure itself in the same way...

--- SpiNNaker-front-end/src/main/java/uk/ac/manchester/spinnaker/front_end/LogControl.java
+++ SpiNNaker-front-end/src/main/java/uk/ac/manchester/spinnaker/front_end/LogControl.java
@@ -16,17 +16,27 @@
  */
 package uk.ac.manchester.spinnaker.front_end;
 
-import static org.apache.log4j.Logger.getRootLogger;
+import static java.lang.System.getProperty;
+import static org.apache.logging.log4j.Level.DEBUG;
+import static org.apache.logging.log4j.Level.ERROR;
+import static org.apache.logging.log4j.Level.INFO;
+import static org.apache.logging.log4j.core.Filter.Result.ACCEPT;
+import static org.apache.logging.log4j.core.Filter.Result.DENY;
+import static org.apache.logging.log4j.core.appender.ConsoleAppender.Target.SYSTEM_ERR;
+import static org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilderFactory.newConfigurationBuilder;
 
 import java.io.File;
-import java.util.concurrent.Executor;
 
-import org.apache.log4j.FileAppender;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-
-import uk.ac.manchester.spinnaker.connections.UDPConnection;
-import uk.ac.manchester.spinnaker.front_end.download.DataGatherer;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.appender.ConsoleAppender;
+import org.apache.logging.log4j.core.appender.FileAppender;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilder;
+import org.apache.logging.log4j.core.config.builder.api.FilterComponentBuilder;
+import org.apache.logging.log4j.core.config.builder.api.LayoutComponentBuilder;
+import org.apache.logging.log4j.core.config.builder.api.LoggerComponentBuilder;
+import org.apache.logging.log4j.core.config.builder.api.RootLoggerComponentBuilder;
+import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
 
 /**
  * Utilities for working with the log. <em>This should be the only place in our
@@ -36,13 +46,87 @@ import uk.ac.manchester.spinnaker.front_end.download.DataGatherer;
  */
 public abstract class LogControl {
 	private static final String LOG_FILE = "jspin.log";
-	private static final String LOGGER_NAME = "tofile";
-	private static final String PARALLEL_LOGGER_NAME = "parallog";
-	private static final String LOGGING_LEVEL_NAME = "logging.level";
-	private static final String UDP_LOGGING = "logging.udp";
-	private static final String EXECUTOR_LOGGING = "logging.executor";
 
 	private LogControl() {
+	}
+
+	private static Level level(String propertyName) {
+		return getProperty(propertyName) == null ? DEBUG : INFO;
+	}
+
+	interface Loggers {
+		String MAIN = "tofile";
+		String CON = "con";
+		String PARALLEL = "parallog";
+	}
+
+	interface Props {
+		String LOGGING_LEVEL_NAME = "logging.level";
+		String UDP_LOGGING = "logging.udp";
+		String EXECUTOR_LOGGING = "logging.executor";
+	}
+
+	interface Classes {
+		String BASE = "uk.ac.manchester.spinnaker.";
+		String DATA_GATHERER = BASE + "front_end.download.DataGatherer";
+		String RR_DATA_GATHERER =
+				BASE + "front_end.download.RecordingRegionDataGatherer";
+		String FAST_EXEC = BASE + "front_end.dse.FastExecuteDataSpecification";
+		String BASE_EXEC = BASE + "front_end.dse.HostExecuteDataSpecification";
+		String UDP_CONN = BASE + "connections.UDPConnection";
+		String EXEC_CORE = BASE + "data_spec.Executor";
+	}
+
+	interface Attrs {
+		String ADDITIVITY = "additivity";
+		String LEVEL = "level";
+		String FILE = "file";
+		String PATTERN = "pattern";
+		String TARGET = "target";
+	}
+
+	interface Patterns {
+		String CON = "%p: %m%n";
+		String MAIN = "%d{HH:mm:ss.SSS} %-5p %c{1}:%L - %m%n";
+		String PARALLEL = "%d{HH:mm:ss.SSS} %-5p %c{1}:%L %X{boardRoot} %m%n";
+	}
+
+	private static FilterComponentBuilder filter(
+			ConfigurationBuilder<?> builder, Level level) {
+		return builder.newFilter("ThresholdFilter", ACCEPT, DENY)
+				.addAttribute(Attrs.LEVEL, level);
+	}
+
+	private static FilterComponentBuilder filter(
+			ConfigurationBuilder<?> builder, String level) {
+		return builder.newFilter("ThresholdFilter", ACCEPT, DENY)
+				.addAttribute(Attrs.LEVEL, level);
+	}
+
+	private static LayoutComponentBuilder layout(
+			ConfigurationBuilder<?> builder, String pattern) {
+		return builder.newLayout("PatternLayout").addAttribute(Attrs.PATTERN,
+				pattern);
+	}
+
+	private static RootLoggerComponentBuilder rootLog(
+			ConfigurationBuilder<?> builder) {
+		return builder.newRootLogger(level(Props.LOGGING_LEVEL_NAME))
+				.add(builder.newAppenderRef(Loggers.MAIN))
+				.add(builder.newAppenderRef(Loggers.CON));
+	}
+
+	private static LoggerComponentBuilder parallelLog(
+			ConfigurationBuilder<?> builder, String name) {
+		return builder.newLogger(name, DEBUG)
+				.add(builder.newAppenderRef(Loggers.PARALLEL))
+				.add(builder.newAppenderRef(Loggers.CON))
+				.addAttribute(Attrs.ADDITIVITY, false);
+	}
+
+	private static LoggerComponentBuilder basicLog(
+			ConfigurationBuilder<?> builder, String name, String prop) {
+		return builder.newLogger(name, level(prop));
 	}
 
 	/**
@@ -52,29 +136,33 @@ public abstract class LogControl {
 	 *            The directory where the log should written inside.
 	 */
 	public static void setLoggerDir(String directoryName) {
-		File dir = new File(directoryName);
-		File logfile = new File(dir, LOG_FILE);
-		FileAppender a =
-				(FileAppender) getRootLogger().getAppender(LOGGER_NAME);
-		if (System.getProperty(LOGGING_LEVEL_NAME) == null) {
-			a.setThreshold(Level.INFO);
-		}
-		a.setFile(logfile.getAbsolutePath());
-		a.activateOptions();
-
-		a = (FileAppender) Logger.getLogger(DataGatherer.class)
-				.getAppender(PARALLEL_LOGGER_NAME);
-		if (System.getProperty(LOGGING_LEVEL_NAME) == null) {
-			a.setThreshold(Level.INFO);
-		}
-		a.setFile(logfile.getAbsolutePath());
-		a.activateOptions();
-
-		if (System.getProperty(UDP_LOGGING) != null) {
-			Logger.getLogger(UDPConnection.class).setLevel(Level.DEBUG);
-		}
-		if (System.getProperty(EXECUTOR_LOGGING) != null) {
-			Logger.getLogger(Executor.class).setLevel(Level.DEBUG);
-		}
+		File logfile = new File(new File(directoryName), LOG_FILE);
+		ConfigurationBuilder<BuiltConfiguration> builder =
+				newConfigurationBuilder();
+		builder.add(rootLog(builder))
+				.add(parallelLog(builder, Classes.DATA_GATHERER))
+				.add(parallelLog(builder, Classes.RR_DATA_GATHERER))
+				.add(parallelLog(builder, Classes.FAST_EXEC))
+				.add(parallelLog(builder, Classes.BASE_EXEC))
+				.add(basicLog(builder, Classes.UDP_CONN, Props.UDP_LOGGING))
+				.add(basicLog(builder, Classes.EXEC_CORE,
+						Props.EXECUTOR_LOGGING))
+				.add(builder
+						.newAppender(Loggers.CON, ConsoleAppender.PLUGIN_NAME)
+						.addAttribute(Attrs.TARGET, SYSTEM_ERR)
+						.add(filter(builder, ERROR))
+						.add(layout(builder, Patterns.CON)))
+				.add(builder.newAppender(Loggers.MAIN, FileAppender.PLUGIN_NAME)
+						.addAttribute(Attrs.FILE, logfile.getAbsolutePath())
+						.add(filter(builder,
+								getProperty(Props.LOGGING_LEVEL_NAME)))
+						.add(layout(builder, Patterns.MAIN)))
+				.add(builder
+						.newAppender(Loggers.PARALLEL, FileAppender.PLUGIN_NAME)
+						.addAttribute(Attrs.FILE, logfile.getAbsolutePath())
+						.add(filter(builder,
+								getProperty(Props.LOGGING_LEVEL_NAME)))
+						.add(layout(builder, Patterns.PARALLEL)));
+		Configurator.initialize(builder.build());
 	}
 }
