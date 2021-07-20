f9fb4bc26ec1497ee640eafe8b82a4bb052c3a21
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2021-07-20 17:13:17 +0100

    Tightening up types

--- SpiNNaker-allocserv/src/main/typescript/spinnaker.ts
+++ SpiNNaker-allocserv/src/main/typescript/spinnaker.ts
@@ -13,18 +13,49 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-interface Foo {
+type BoardTriad = readonly [number, number, number];
+type HexCoords = readonly [number, number][];
+
+interface BoardLocator {
+	triad: {
+		x: number;
+		y: number;
+		z: number;
+	};
+	physical: {
+		cabinet: number;
+		frame: number;
+		board: number;
+	};
+	network: {
+		address: string;
+	};
+};
+
+interface MachineJobDescriptor {
+	id: number;
+	url?: string;
+	owner?: string;
+	boards: BoardLocator[];
+};
+
+interface MachineDescriptor {
+	name: string;
 	width: number;
 	height: number;
+	num_in_use: number;
+	tags: string[];
+	jobs: MachineJobDescriptor[];
+	// TODO list dead boards too
 };
 
 function drawBoard(
 		ctx : CanvasRenderingContext2D,
 		x : number, y : number, scale : number,
 		fill : boolean = false,
-		label : string = undefined) : number[][] {
+		label : string = undefined) : HexCoords {
 	ctx.beginPath();
-	var coords : number[][] = [];
+	var coords : [number, number][] = [];
 	coords.push([x, y]);
 	ctx.moveTo(x, y);
 	coords.push([x + scale, y]);
@@ -41,20 +72,24 @@ function drawBoard(
 	if (fill) {
 		ctx.fill();
 	}
-	if (label !== undefined) {
-		ctx.textAlign = "center";
-		ctx.fillText(label, x + scale, y - scale);
-	}
 	ctx.stroke();
+	if (label !== undefined) {
+		ctx.save();
+		ctx.fillStyle = ctx.strokeStyle;
+		ctx.textAlign = "center";
+		ctx.fillText(label, x + scale, y - scale, 2 * scale);
+		ctx.restore();
+	}
 	return coords;
 };
 
 function drawTriadBoard(
 		ctx : CanvasRenderingContext2D,
 		rootX : number, rootY : number, scale : number,
-		x : number, y : number, z : number,
+		key : BoardTriad,
 		fill : boolean = false,
-		labeller : (x:number, y:number, z:number) => string = undefined) : number[][] {
+		labeller : (key: BoardTriad) => string = undefined) : HexCoords {
+	const [x, y, z] = key;
 	var bx : number = rootX + x * 3 * scale;
 	var by : number = rootY - y * 3 * scale;
 	if (z == 1) {
@@ -66,7 +101,7 @@ function drawTriadBoard(
 	}
 	var label : string = undefined;
 	if (labeller !== undefined) {
-		label = labeller(x, y, z);
+		label = labeller(key);
 	}
 	return drawBoard(ctx, bx, by, scale, fill, label);
 };
@@ -75,19 +110,20 @@ function drawLayout(
 		ctx : CanvasRenderingContext2D,
 		rootX : number, rootY : number, scale : number,
 		width : number, height : number, depth : number = 3,
-		colourer : string | ((x:number,y:number,z:number) => string) = undefined,
-		labeller : (x:number, y:number, z:number) => string = undefined) : Map<number[],number[][]> {
-	var tloc : Map<number[],number[][]> = new Map();
+		colourer : string | ((key: BoardTriad) => string) = undefined,
+		labeller : (key: BoardTriad) => string = undefined) : Map<BoardTriad,HexCoords> {
+	var tloc : Map<BoardTriad,HexCoords> = new Map();
 	for (var y : number = 0; y < height; y++) {
 		for (var x : number = 0; x < width; x++) {
-			for (var z : number = 0; z <= depth; z++) {
+			for (var z : number = 0; z < depth; z++) {
+				const key = [x, y, z] as const;
 				if (typeof colourer === 'string') {
 					ctx.fillStyle = colourer;
 				} else {
-					ctx.fillStyle = colourer(x, y, z);
+					ctx.fillStyle = colourer(key);
 				}
-				tloc.set([x,y,z], drawTriadBoard(
-						ctx, rootX, rootY, scale, x, y, z, true, labeller));
+				tloc.set(key, drawTriadBoard(
+						ctx, rootX, rootY, scale, key, true, labeller));
 			}
 		}
 	}
@@ -97,18 +133,18 @@ function drawLayout(
 // https://stackoverflow.com/a/29915728/301832
 function inside(
 		x : number, y : number,
-		tloc : Map<number[],number[][]>) : number[] {
+		tloc : Map<BoardTriad,HexCoords>) : BoardTriad {
     // ray-casting algorithm based on
     // https://wrf.ecse.rpi.edu/Research/Short_Notes/pnpoly.html/pnpoly.html
 
-	for (const key of tloc) {
-		var triad = key[0], poly = key[1];
+	for (const item of tloc) {
+		const [triad, poly] = item;
 	    var inside = false;
 	    for (var i = 0, j = poly.length - 1; i < poly.length; j = i++) {
-	        var xi = poly[i][0], yi = poly[i][1];
-	        var xj = poly[j][0], yj = poly[j][1];
+	        const [xi, yi] = poly[i];
+	        const [xj, yj] = poly[j];
 
-	        var intersect = ((yi > y) != (yj > y))
+	        const intersect = ((yi > y) != (yj > y))
 	            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
 	        if (intersect) {
 				inside = !inside;
@@ -124,39 +160,40 @@ function inside(
 
 function initialDrawAllocation(
 		canvasId : string,
-		descriptor : Foo) {
-	var canv = <HTMLCanvasElement> document.getElementById(canvasId);
-	var rootX = 5;
-	var rootY = canv.height - 5;
-	var scaleX : number = (canv.width - 10) / (descriptor.width * 3 + 1);
-	var scaleY : number = (canv.height - 10) / (descriptor.height * 3 + 1);
-	var scale = (scaleX < scaleY) ? scaleX : scaleY;
-	var ctx : CanvasRenderingContext2D = canv.getContext("2d");
+		descriptor : MachineDescriptor) {
+	const canv = <HTMLCanvasElement> document.getElementById(canvasId);
+	const rootX = 5;
+	const rootY = canv.height - 5;
+	const scaleX : number = (canv.width - 10) / (descriptor.width * 3 + 1);
+	const scaleY : number = (canv.height - 10) / (descriptor.height * 3 + 1);
+	const scale = (scaleX < scaleY) ? scaleX : scaleY;
+	const ctx : CanvasRenderingContext2D = canv.getContext("2d");
+
 	ctx.strokeStyle = 'black';
-	var tloc = drawLayout(ctx, rootX, rootY, scale,
+	function lbl(key : BoardTriad) {
+		const [x, y, z] = key;
+		return "(" + x + "," + y + "," + z + ")";
+	}
+	const tloc = drawLayout(ctx, rootX, rootY, scale,
 			descriptor.width, descriptor.height, 3,
-			"white",
-			(x, y, z) => "(" + x + "," + y + "," + z + ")");
-	var current : number[] = undefined;
+			"white", lbl);
+	var current : BoardTriad = undefined;
 	function motion(e : MouseEvent) {
 		const x = e.offsetX, y = e.offsetY;
-		var triad = inside(x, y, tloc);
+		const triad = inside(x, y, tloc);
 		if (triad !== undefined) {
 			if (current !== undefined) {
 				if (current === triad) {
 					return;
 				}
 				ctx.strokeStyle = 'black';
-				drawTriadBoard(ctx, rootX, rootY, scale,
-						current[0], current[1], current[2]);
+				drawTriadBoard(ctx, rootX, rootY, scale, current);
 			}
 			ctx.strokeStyle = 'red';
-			drawTriadBoard(ctx, rootX, rootY, scale,
-					triad[0], triad[1], triad[2])
+			drawTriadBoard(ctx, rootX, rootY, scale, triad);
 		} else if (current !== undefined) {
 			ctx.strokeStyle = 'black';
-			drawTriadBoard(ctx, rootX, rootY, scale,
-					current[0], current[1], current[2]);
+			drawTriadBoard(ctx, rootX, rootY, scale, current);
 		}
 		current = triad;
 	}
