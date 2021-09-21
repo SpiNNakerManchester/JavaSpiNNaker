bdd4c0f867445579f3fdb0530719fd0a728f484a
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2021-09-21 15:52:27 +0100

    Documenting code for my own sanity

--- SpiNNaker-allocserv/src/main/typescript/spinnaker.ts
+++ SpiNNaker-allocserv/src/main/typescript/spinnaker.ts
@@ -13,58 +13,106 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+/** Machine logical global coordinate space (x, y, z). */
 type BoardTriad = readonly [number, number, number];
+/** The list of canvas coordinates of a hexagon representing a board. */
 type HexCoords = readonly [number, number][];
 
+/** The various forms of board locations spat out by the server. */
 interface BoardLocator {
+	/** Logical board coordinates. */
 	triad: {
 		x: number;
 		y: number;
 		z: number;
 	};
+	/** Physical board coordinates. */
 	physical: {
 		cabinet: number;
 		frame: number;
 		board: number;
 	};
+	/** Network address coordinates. */
 	network: {
 		address: string;
 	};
 };
 
+/** Briefly describes a job on a machine. */
 interface MachineJobDescriptor {
+	/** The job ID. */
 	id: number;
+	/** The address of the job details page. */
 	url?: string;
+	/** The owner of the job, if the current user is allowed to see that. */
 	owner?: string;
+	/** The boards allocated to the job. */
 	boards: BoardLocator[];
 };
 
+/** Describes a machine. */
 interface MachineDescriptor {
+	/** The name of the machine. */
 	name: string;
+	/** The width of the machine, in triads. */
 	width: number;
+	/** The height of the machine, in triads. */
 	height: number;
+	/** The number of boards currently in use. */
 	num_in_use: number;
+	/** The tags of the machine. */
 	tags: string[];
+	/** The jobs on the machine. */
 	jobs: MachineJobDescriptor[];
+	/** The boards that can have jobs running on them. */
 	live_boards: BoardLocator[];
+	/** The boards that are out of service. */
 	dead_boards: BoardLocator[];
 };
 
+/** Describes a job. */
 interface JobDescriptor {
+	/** The job ID. */
 	id: number;
+	/** The owner of the job, if the current user is allowed to see that. */
 	owner?: string;
+	/** The state of the job. */
 	state: string;
-	start: string; // Instant
-	keep_alive: string; // Duration
+	/** The instant that the job started. */
+	start: string;
+	/** The keep-alive period. */
+	keep_alive: string;
+	/**
+	 * The host that is keeping the job alive, if the current user is allowed
+	 * to see that.
+	 */
 	owner_host?: string;
+	/**
+	 * The width of the job's allocation in boards, if an allocation has been
+	 * made.
+	 */
 	width?: number;
+	/**
+	 * The height of the job's allocation in boards, if an allocation has been
+	 * made.
+	 */
 	height?: number;
+	/** Whether the job's allocated boards are switched on. */
 	powered: boolean;
+	/** What machine is the job using. */
 	machine: string;
+	/** Address of the machine information page. */
 	machine_url: string;
+	/** The list of boards allocated to the job. */
 	boards: BoardLocator[];
+	/**
+	 * The original job request message, if the current user is allowed
+	 * to see that.
+	 */
 	request?: any;
+	/** The width of the job's allocation, in triads. */
 	triad_width: number;
+	/** The height of the job's allocation, in triads. */
 	triad_height: number;
 };
 
@@ -166,6 +214,7 @@ function drawTriadBoard(
 
 /**
  * Convert board triad coordinates into a key suitable for a Map.
+ *
  * @param {BoardCoords} coords
  *		The coordinates.
  * @return {string}
@@ -176,6 +225,30 @@ function tuplekey(coords: BoardTriad) : string {
 	return x + "," + y + "," + z;
 }
 
+/**
+ * Convert a list of board locators into a map using the triad coords as key.
+ *
+ * @param {CanvasRenderingContext2D} ctx
+ *		How to draw on the canvas.
+ * @param {number} rootX
+ *		The root X coordinate for drawing.
+ * @param {number} rootY
+ *		The root Y coordinate for drawing.
+ * @param {number} scale
+ *		The diagram basic scaling factor.
+ * @param {number} width
+ *		The width of the diagram, in triads.
+ * @param {number} height
+ *		The height of the diagram, in triads.
+ * @param {number} depth
+ *		The depth of the diagram (usually 1 or 3).
+ * @param {string | ((key: BoardTriad) => string)} colourer
+ *		How to get a colour for a board.
+ * @param {(key: BoardTriad) => string} labeller
+ *		How to get the label for a board.
+ * @return {Map<string,[BoardTriad,HexCoords]>}
+ *		Description of where each board was drawn.
+ */
 function drawLayout(
 		ctx : CanvasRenderingContext2D,
 		rootX : number, rootY : number, scale : number,
@@ -200,6 +273,18 @@ function drawLayout(
 	return tloc;
 }
 
+/**
+ * Test if a point is inside a board.
+ *
+ * @param {number} x
+ *		Canvas X coordinate
+ * @param {number} y
+ *		Canvas Y coordinate
+ * @param {Map<string,[BoardTriad,HexCoords]>} tloc
+ *		Location mapping from `drawLayout()`
+ * @return {BoardTriad}
+ *		Which board the point is in, or `undefined` if none.
+ */
 // https://stackoverflow.com/a/29915728/301832
 function inside(
 		x : number, y : number,
@@ -228,6 +313,14 @@ function inside(
     return undefined;
 }
 
+/**
+ * Convert a list of board locators into a map using the triad coords as key.
+ *
+ * @param {BoardLocator[]} boards
+ *		The name of the main canvas element.
+ * @return {Map<string,BoardLocator>}
+ *		Map from triad coords to full locators,
+ */
 function boardMap(boards: readonly BoardLocator[]) : Map<string,BoardLocator> {
 	var m : Map<string,BoardLocator> = new Map();
 	for (const b of boards) {
@@ -237,11 +330,30 @@ function boardMap(boards: readonly BoardLocator[]) : Map<string,BoardLocator> {
 	return m;
 }
 
+/**
+ * Shared part of the tooltip control code.
+ *
+ * @param {HTMLCanvasElement} canv
+ *		The main canvas element.
+ * @param {HTMLCanvasElement} tooltip
+ *		The tooltip canvas element.
+ * @param {CanvasRenderingContext2D} tooltipCtx
+ *		The drawing context for the tooltip.
+ * @param {((t: BoardTriad) => HexCoords)} locmapper
+ *		How to look up coordinates.
+ * @param {number} scale
+ *		General UI scaling factor.
+ * @param {BoardTriad?} triad
+ *		Which triad (if any) are we setting the tooltip for.
+ * @param {string?} message
+ *		What message (if any) are we setting the tooltip to be. If omitted,
+ *		we clear the tooltip. May be multiline.
+ */
 function setTooltipCore(
 		canv: HTMLCanvasElement, tooltip: HTMLCanvasElement,
 		tooltipCtx: CanvasRenderingContext2D,
 		locmapper: ((t: BoardTriad) => HexCoords),
-		scale:number, triad?: BoardTriad, message?: string) {
+		scale: number, triad?: BoardTriad, message?: string) {
 	if (triad === undefined || message === undefined) {
 		tooltip.style.left = "-2000px";
 	} else {
@@ -262,7 +374,16 @@ function setTooltipCore(
 	}
 }
 
-
+/**
+ * Set up a canvas to illustrate a machine's boards.
+ *
+ * @param {string} canvasId
+ *		The name of the main canvas element.
+ * @param {string} tooltipId
+ *		The name of the tooltip canvas element.
+ * @param {MachineDescriptor} descriptor
+ *		The information loaded about the machine.
+ */
 function drawMachine(
 		canvasId : string, tooltipId : string,
 		descriptor : MachineDescriptor) {
@@ -282,38 +403,62 @@ function drawMachine(
 
 	ctx.strokeStyle = 'black';
 
-	function lbl(key : BoardTriad) : string {
-		const [x, y, z] = key;
-		if (dead.has(tuplekey(key))) {
-			return `\u2620 (${x},${y},${z})`;
-		} else {
-			return `(${x},${y},${z})`;
-		}
-	}
-
-	function clr(key: BoardTriad) : string {
-		const k = tuplekey(key);
-		if (dead.has(k)) {
-			return "#444";
-		} else if (live.has(k)) {
-			return "white";
-		} else {
-			return "black";
-		}
-	}
-
+	/** Where all the boards are on the canvas. */
 	const tloc = drawLayout(ctx, rootX, rootY, scale,
 			descriptor.width, descriptor.height, 3,
-			clr, lbl);
+			(key: BoardTriad) => {
+				// Colour selection function
+				const k = tuplekey(key);
+				if (dead.has(k)) {
+					return "#444";
+				} else if (live.has(k)) {
+					return "white";
+				} else {
+					return "black";
+				}
+			}, (key : BoardTriad) => {
+				// Label construction function
+				const [x, y, z] = key;
+				if (dead.has(tuplekey(key))) {
+					return `\u2620 (${x},${y},${z})`;
+				} else {
+					return `(${x},${y},${z})`;
+				}
+			});
+	/**
+	 * Get the canvas coordinates of a board's hexagon. Wrapper around `tloc`.
+	 *
+	 * @param {BoardTriad} triad
+	 *		The logical coordinates of the board.
+	 * @return {HexCoords}
+	 *		The hex coordinates, or `undefined` if the board isn't known.
+	 */
 	function location(triad: BoardTriad) : HexCoords {
 		const t = tloc.get(tuplekey(triad));
 		return t == undefined ? undefined : t[1];
 	}
 
+	/**
+	 * Set or clear a tooltip.
+	 *
+	 * @param {BoardTriad?} triad
+	 *		The logical coordinates of the board.
+	 *		Clear the tooltip if `undefined`.
+	 * @param {string?} message
+	 *		The tooltip message. Clear the tooltip if `undefined`.
+	 */
 	function setTooltip(triad?: BoardTriad, message?: string) {
 		setTooltipCore(canv, tooltip, tooltipCtx, location, scale, triad, message);
 	}
 
+	/**
+	 * Get the detailed description of a board.
+	 *
+	 * @param {BoardTriad} triad
+	 *		Which board to describe.
+	 * @return {string}
+	 *		The multiline description of the board.
+	 */
 	function triadDescription(triad: BoardTriad) : string {
 		const [x, y, z] = triad;
 		const key = tuplekey(triad);
@@ -335,13 +480,16 @@ function drawMachine(
 		return s;
 	}
 
+	/** The current board (i.e., that has the mouse over it). */
 	var current : BoardTriad = undefined;
+	/** Common code: clear the current board/tooltip. */
 	function clearCurrent() {
 		ctx.strokeStyle = 'black';
 		drawTriadBoard(ctx, rootX, rootY, scale, current);
 		setTooltip();
 		current = undefined;
 	}
+	/** Common code: set the current board/tooltip. */
 	function setCurrent(triad : BoardTriad) {
 		ctx.strokeStyle = 'red';
 		drawTriadBoard(ctx, rootX, rootY, scale, triad);
@@ -386,6 +534,15 @@ function drawMachine(
 	});
 };
 
+/**
+ * Set up a canvas to illustrate a job's allocation.
+ * @param {string} canvasId
+ *		The name of the main canvas element.
+ * @param {string} tooltipId
+ *		The name of the tooltip canvas element.
+ * @param {JobDescriptor} descriptor
+ *		The information loaded about the job.
+ */
 function drawJob(
 		canvasId : string, tooltipId : string,
 		descriptor : JobDescriptor) {
@@ -404,28 +561,56 @@ function drawJob(
 
 	ctx.strokeStyle = 'black';
 	const {x: rx, y: ry} = descriptor.boards[0].triad;
+	/** Where all the boards are on the canvas. */
 	const tloc = drawLayout(ctx, rootX, rootY, scale,
 			descriptor.triad_width, descriptor.triad_height, 3,
 			triadCoord => {
+				// Colour selection function
 				const [x, y, z] = triadCoord;
 				if (allocated.has(tuplekey([rx+x, ry+y, z]))) {
 					return "yellow";
 				}
 				return "white";
 			}, triadCoord => {
+				// Label construction function
 				const [x, y, z] = triadCoord;
 				return `(${x},${y},${z})`;
 			});
+	/**
+	 * Get the canvas coordinates of a board's hexagon. Wrapper around `tloc`.
+	 *
+	 * @param {BoardTriad} triad
+	 *		The logical coordinates of the board.
+	 * @return {HexCoords}
+	 *		The hex coordinates, or `undefined` if the board isn't known.
+	 */
 	function location(triad: BoardTriad) : HexCoords {
 		const t = tloc.get(tuplekey(triad));
 		return t == undefined ? undefined : t[1];
 	}
 
+	/**
+	 * Set or clear a tooltip.
+	 *
+	 * @param {BoardTriad?} triad
+	 *		The logical coordinates of the board.
+	 *		Clear the tooltip if `undefined`.
+	 * @param {string?} message
+	 *		The tooltip message. Clear the tooltip if `undefined`.
+	 */
 	function setTooltip(triad?: BoardTriad, message?: string) {
 		setTooltipCore(canv, tooltip, tooltipCtx, location, scale, triad,
 				message);
 	}
 
+	/**
+	 * Get the detailed description of a board.
+	 *
+	 * @param {BoardTriad} triad
+	 *		Which board to describe.
+	 * @return {string}
+	 *		The multiline description of the board.
+	 */
 	function triadDescription(triad: BoardTriad) : string {
 		const [x, y, z] = triad;
 		// FIXME define what should go in the tooltip
@@ -433,13 +618,16 @@ function drawJob(
 		return s;
 	}
 
+	/** The current board (i.e., that has the mouse over it). */
 	var current : BoardTriad = undefined;
+	/** Common code: clear the current board/tooltip. */
 	function clearCurrent() {
 		ctx.strokeStyle = 'black';
 		drawTriadBoard(ctx, rootX, rootY, scale, current);
 		setTooltip();
 		current = undefined;
 	}
+	/** Common code: set the current board/tooltip. */
 	function setCurrent(triad : BoardTriad) {
 		ctx.strokeStyle = 'green';
 		drawTriadBoard(ctx, rootX, rootY, scale, triad);
