d8c4dde5166562df56820638bf6f6c0c09b9fcf0
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2021-10-20 08:31:50 +0100

    Fix a wrong operation name in an error message

--- SpiNNaker-comms/src/main/java/uk/ac/manchester/spinnaker/messages/bmp/GetBMPVersion.java
+++ SpiNNaker-comms/src/main/java/uk/ac/manchester/spinnaker/messages/bmp/GetBMPVersion.java
@@ -47,7 +47,7 @@ public class GetBMPVersion extends BMPRequest<GetBMPVersion.Response> {
 
 		private Response(ByteBuffer buffer)
 				throws UnexpectedResponseCodeException {
-			super("Read ADC", CMD_VER, buffer);
+			super("Read Version", CMD_VER, buffer);
 			versionInfo = new VersionInfo(buffer, true);
 		}
 	}
