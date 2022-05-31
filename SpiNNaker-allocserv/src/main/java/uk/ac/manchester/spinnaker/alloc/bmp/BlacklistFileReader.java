5c2dd5130b69f331d7cf2f11e3a2f79e1a4acc19
Donal K. Fellows <donal.k.fellows@manchester.ac.uk>
2022-05-31 13:59:54 +0100

    Javadoc

--- SpiNNaker-allocserv/src/main/java/uk/ac/manchester/spinnaker/alloc/bmp/BlacklistFileReader.java
+++ SpiNNaker-allocserv/src/main/java/uk/ac/manchester/spinnaker/alloc/bmp/BlacklistFileReader.java
@@ -17,6 +17,7 @@
 package uk.ac.manchester.spinnaker.alloc.bmp;
 
 import static java.lang.Integer.parseInt;
+import static java.util.Arrays.stream;
 import static java.util.regex.Pattern.compile;
 import static java.util.stream.Collectors.toSet;
 
@@ -24,7 +25,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -37,46 +37,79 @@ import uk.ac.manchester.spinnaker.machine.ChipLocation;
 import uk.ac.manchester.spinnaker.machine.Direction;
 import uk.ac.manchester.spinnaker.messages.bmp.Blacklist;
 
+/**
+ * Read a blacklist from a definition file. Note that the code does not examine
+ * the filename, which is often used to determine what board the blacklist
+ * applies to; that is left as a problem for the caller to handle.
+ *
+ * @author Donal Fellows
+ */
 public class BlacklistFileReader {
-	public Blacklist readBlacklistFile(File f) throws IOException {
-		try (FileReader r = new FileReader(f);
+	/**
+	 * Read a blacklist from a file.
+	 *
+	 * @param file
+	 *           The file to read from.
+	 * @return The parsed blacklist.
+	 * @throws IOException
+	 *           If the file can't be read from.
+	 * @throws IllegalArgumentException
+	 *           If the file is badly formatted.
+	 */
+	public Blacklist readBlacklistFile(File file) throws IOException {
+		try (FileReader r = new FileReader(file);
 				BufferedReader br = new BufferedReader(r)) {
 			return readBlacklist(br);
 		}
 	}
 
-	private static Pattern chipPattern = compile(
+	// REs from Perl code to read blacklist files
+
+	private static final Pattern CHIP_PATTERN = compile(
 			"chip\\s+(?<x>[0-7])\\s+(?<y>[0-7])\\s+(?<rest>.+)$");
 
-	private static Pattern corePattern = compile("core\\s+(?<cores>\\S+)\\s*");
+	private static final Pattern CORE_PATTERN = compile(
+			"core\\s+(?<cores>\\S+)\\s*");
 
-	private static Pattern linkPattern = compile("link\\s+(?<links>\\S+)\\s*");
+	private static final Pattern LINK_PATTERN = compile(
+			"link\\s+(?<links>\\S+)\\s*");
 
-	private static Pattern deadPattern = compile("dead\\s*");
+	private static final Pattern DEAD_PATTERN = compile("dead\\s*");
 
 	private static String deleteMatched(Matcher m) {
-		// 8 uses StringBuffer for this; WHYWHYWHY?!
+		// Java 8 uses StringBuffer for this; WHYWHYWHY?!
 		StringBuffer sb = new StringBuffer();
 		m.appendReplacement(sb, "").appendTail(sb);
 		return sb.toString();
 	}
 
 	private static Set<Integer> parseCommaSeparatedSet(String str) {
-		return Arrays.stream(str.split(",")).map(Integer::parseInt)
-				.collect(toSet());
+		return stream(str.split(",")).map(Integer::parseInt).collect(toSet());
 	}
 
 	private static <T> Set<T> parseCommaSeparatedSet(String str,
 			Function<Integer, T> fun) {
-		return Arrays.stream(str.split(",")).map(Integer::parseInt).map(fun)
+		return stream(str.split(",")).map(Integer::parseInt).map(fun)
 				.collect(toSet());
 	}
 
+	/**
+	 * Read a blacklist from an input reader.
+	 *
+	 * @param r
+	 *           The reader to read from.
+	 * @return The parsed blacklist.
+	 * @throws IOException
+	 *           If the reader can't be read from.
+	 * @throws IllegalArgumentException
+	 *           If the reader's content is badly formatted.
+	 */
 	public Blacklist readBlacklist(BufferedReader r) throws IOException {
-		String line;
 		Map<ChipLocation, Set<Integer>> deadCores = new HashMap<>();
 		Map<ChipLocation, Set<Direction>> deadLinks = new HashMap<>();
 		Set<ChipLocation> deadChips = new HashSet<>();
+
+		String line;
 		while ((line = r.readLine()) != null) {
 			line = line.trim();
 			if (line.isEmpty() || line.startsWith("#")) {
@@ -85,13 +118,26 @@ public class BlacklistFileReader {
 			}
 			parseLine(line, deadChips, deadCores, deadLinks);
 		}
+
 		return new Blacklist(deadChips, deadCores, deadLinks);
 	}
 
+	/**
+	 * Parse one non-empty non-comment line of a blacklist file.
+	 *
+	 * @param line
+	 *            The line's contents.
+	 * @param deadChips
+	 *            Where to accumulate dead chips.
+	 * @param deadCores
+	 *            Where to accumulate dead cores.
+	 * @param deadLinks
+	 *            Where to accumulate dead links.
+	 */
 	private void parseLine(String line, Set<ChipLocation> deadChips,
 			Map<ChipLocation, Set<Integer>> deadCores,
 			Map<ChipLocation, Set<Direction>> deadLinks) {
-		Matcher m = chipPattern.matcher(line);
+		Matcher m = CHIP_PATTERN.matcher(line);
 		if (!m.matches()) {
 			return;
 		}
@@ -101,21 +147,21 @@ public class BlacklistFileReader {
 		String rest = m.group("rest");
 
 		Set<Integer> cores = null;
-		m = corePattern.matcher(rest);
+		m = CORE_PATTERN.matcher(rest);
 		if (m.find()) {
 			cores = parseCommaSeparatedSet(m.group("cores"));
 			rest = deleteMatched(m);
 		}
 
 		Set<Direction> links = null;
-		m = linkPattern.matcher(rest);
+		m = LINK_PATTERN.matcher(rest);
 		if (m.find()) {
 			links = parseCommaSeparatedSet(m.group("links"), Direction::byId);
 			rest = deleteMatched(m);
 		}
 
 		boolean dead = false;
-		m = deadPattern.matcher(rest);
+		m = DEAD_PATTERN.matcher(rest);
 		if (m.find()) {
 			dead = true;
 			rest = deleteMatched(m);
