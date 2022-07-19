e3e0f6d4a09c60e2d44a7a766d55e352ffead7f0
Donal K. Fellows <donal.k.fellows@manchester.ac.uk>
2022-07-19 12:20:58 +0100

    Improve some tests

--- SpiNNaker-py2json/src/test/java/uk/ac/manchester/spinnaker/py2json/TestConvert.java
+++ SpiNNaker-py2json/src/test/java/uk/ac/manchester/spinnaker/py2json/TestConvert.java
@@ -16,73 +16,100 @@
  */
 package uk.ac.manchester.spinnaker.py2json;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.skyscreamer.jsonassert.JSONCompareMode.STRICT;
 import static uk.ac.manchester.spinnaker.py2json.MachineDefinitionConverter.getJsonWriter;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
 
+import org.apache.commons.io.IOUtils;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 
 class TestConvert {
-	private static final int BUFFER_SIZE = 1024;
-
-	private String readFile(String filename) throws IOException {
-		try (var i = getClass().getClassLoader().getResourceAsStream(filename);
-				var isr = new InputStreamReader(i)) {
-			var sb = new StringBuilder();
-			var buffer = new char[BUFFER_SIZE];
-			while (true) {
-				int len = isr.read(buffer);
-				if (len < 0) {
-					break;
-				}
-				sb.append(buffer, 0, len);
-			}
-			return sb.toString();
-		}
+	/**
+	 * Shortening.
+	 *
+	 * @param filename
+	 *            The local filename of the (test) resource.
+	 * @return The URL of the resource.
+	 */
+	private static URL getResource(String filename) {
+		return TestConvert.class.getResource(filename);
 	}
 
-	private File getFile(String filename) {
-		var f = new File(
-				getClass().getClassLoader().getResource(filename).getFile());
+	/**
+	 * Get a resource as a file handle. <strong>Also sets {@code user.dir} to
+	 * the directory containing the file!</strong> Without that, the Python code
+	 * can't find any auxiliary files, such as the CSV.
+	 *
+	 * @param filename
+	 *            The local filename of the (test) resource.
+	 * @return The handle of the file.
+	 */
+	private static File getFile(String filename) {
+		var f = new File(getResource(filename).getFile());
+		// Ugh!
 		System.setProperty("user.dir", f.getAbsoluteFile().getParent());
 		return f;
 	}
 
+	private static final String SINGLE_BOARD = "single_board.py";
+
+	private static final String CSV_DEFINED = "from_csv.py";
+
+	private static final String EXPECTED_JSON = "expected.json";
+
 	@Test
 	void testReadPythonSingleBoard() {
-		var f = getFile("single_board.py");
-		Configuration c;
+		var f = getFile(SINGLE_BOARD);
 		try (var mdl = new MachineDefinitionConverter()) {
-			c = mdl.loadClassicConfigurationDefinition(f, false);
+			var c = mdl.loadClassicConfigurationDefinition(f, false);
+			assertEquals(1, c.machines.size());
+			var machine = c.machines.get(0);
+			assertNotNull(machine);
+			assertEquals("my-board", machine.name);
+			assertEquals(1, machine.boardLocations.size());
 		}
-		assertNotNull(c.machines.get(0));
 	}
 
 	@Test
 	void testReadPythonFromCSV() {
-		var f = getFile("from_csv.py");
-		Configuration c;
+		var f = getFile(CSV_DEFINED);
 		try (var mdl = new MachineDefinitionConverter()) {
-			c = mdl.loadClassicConfigurationDefinition(f, true);
+			var c = mdl.loadClassicConfigurationDefinition(f, true);
+			assertEquals(1, c.machines.size());
+			var machine = c.machines.get(0);
+			assertNotNull(machine);
+			assertEquals("SpiNNaker1M", machine.name);
+			// This is actually cut down a lot from the real 1M machine
+			assertEquals(3, machine.boardLocations.size());
+			assertEquals(Set.of(new XYZ(0, 0, 0), new XYZ(0, 0, 1),
+					new XYZ(0, 0, 2)), machine.boardLocations.keySet());
+			assertEquals(1, machine.bmpIPs.size());
+			assertEquals(3, machine.spinnakerIPs.size());
+			assertEquals(Set.of("10.11.193.1", "10.11.193.17", "10.11.193.9"),
+					new HashSet<>(machine.spinnakerIPs.values()));
+			assertEquals(machine.boardLocations.keySet(),
+					machine.spinnakerIPs.keySet());
 		}
-		assertNotNull(c.machines.get(0));
 	}
 
 	@Test
 	void testProduceJSON() throws IOException, JSONException {
-		var expectedJson = readFile("expected.json");
-		var f = getFile("single_board.py");
-		String json;
+		var expectedJson = IOUtils.toString(getResource(EXPECTED_JSON), UTF_8);
+		var f = getFile(SINGLE_BOARD);
 		try (var mdl = new MachineDefinitionConverter()) {
 			var c = mdl.loadClassicConfigurationDefinition(f, true);
-			json = getJsonWriter().writeValueAsString(c);
+			var json = getJsonWriter().writeValueAsString(c);
+			assertNotNull(json);
+			JSONAssert.assertEquals(expectedJson, json, STRICT);
 		}
-		assertNotNull(json);
-		JSONAssert.assertEquals(expectedJson, json, true);
 	}
 }
