c552e21f860e992e529dd2878750b03154a04f0e
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2023-01-25 14:22:29 +0000

    Add license

--- mkpasswd.sh
+++ mkpasswd.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Copyright (c) 2022 The University of Manchester
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 set -- $(locale LC_MESSAGES)
 yesexpr="$1"; noexpr="$2";
 read -p "New username: " USERNAME
@@ -6,9 +20,9 @@ read -p "New password: " -s PASSWORD
 echo ""
 while true; do
     read -p "Admin (Yy/Nn)? " ADMIN
-    if [[ $ADMIN =~ $yesexpr ]]; then 
+    if [[ $ADMIN =~ $yesexpr ]]; then
         USERTRUST=3
-        break; 
+        break;
     fi
     if [[ $ADMIN =~ $noexpr ]]; then
         USERTRUST=2
