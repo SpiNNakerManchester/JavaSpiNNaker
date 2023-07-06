d0cb45f14c33f5c5d2a573a338e48b1dad370dc1
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2023-07-06 16:21:57 +0100

    Use the correct role

--- SpiNNaker-allocserv/src/main/java/uk/ac/manchester/spinnaker/alloc/web/SpallocServiceImpl.java
+++ SpiNNaker-allocserv/src/main/java/uk/ac/manchester/spinnaker/alloc/web/SpallocServiceImpl.java
@@ -281,7 +281,7 @@ public class SpallocServiceImpl extends BackgroundSupport
 		}
 
 		if (!security.isUserInRole("ADMIN")
-				&& !security.isUserInRole("NMPI_USER")
+				&& !security.isUserInRole("NMPI_EXEC")
 				&& !isNull(req.owner) && !req.owner.isBlank()) {
 			throw new BadArgs("Only admin and NMPI users can specify an owner");
 		}
