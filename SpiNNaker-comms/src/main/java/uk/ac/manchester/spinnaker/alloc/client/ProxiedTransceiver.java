3e1e5c3a3f17d8094d392a51a53c9a972e34c48d
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2025-02-14 16:31:39 +0000

    Put this in the right place and fill in the gaps

--- SpiNNaker-comms/src/main/java/uk/ac/manchester/spinnaker/alloc/client/ProxiedTransceiver.java
+++ SpiNNaker-comms/src/main/java/uk/ac/manchester/spinnaker/alloc/client/ProxiedTransceiver.java
@@ -15,82 +15,190 @@
  */
 package uk.ac.manchester.spinnaker.alloc.client;
 
+import static uk.ac.manchester.spinnaker.machine.ChipLocation.ZERO_ZERO;
+import static uk.ac.manchester.spinnaker.utils.InetFactory.getByNameQuietly;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.Inet4Address;
+import java.io.InputStream;
 import java.net.InetAddress;
-import java.util.Collection;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import com.google.errorprone.annotations.CheckReturnValue;
 import com.google.errorprone.annotations.MustBeClosed;
 
+import uk.ac.manchester.spinnaker.alloc.client.SpallocClient.Job;
 import uk.ac.manchester.spinnaker.connections.EIEIOConnection;
 import uk.ac.manchester.spinnaker.connections.SCPConnection;
 import uk.ac.manchester.spinnaker.connections.model.Connection;
 import uk.ac.manchester.spinnaker.machine.ChipLocation;
+import uk.ac.manchester.spinnaker.machine.HasCoreLocation;
 import uk.ac.manchester.spinnaker.machine.MachineVersion;
+import uk.ac.manchester.spinnaker.machine.MemoryLocation;
+import uk.ac.manchester.spinnaker.spalloc.exceptions.SpallocServerException;
+import uk.ac.manchester.spinnaker.transceiver.ParallelSafe;
+import uk.ac.manchester.spinnaker.transceiver.ProcessException;
 import uk.ac.manchester.spinnaker.transceiver.SpinnmanException;
 import uk.ac.manchester.spinnaker.transceiver.Transceiver;
 
 /** A transceiver that routes messages across the proxy. */
 final class ProxiedTransceiver extends Transceiver {
-	private final ProxyProtocolClient websocket;
 
-	private final Map<Inet4Address, ChipLocation> hostToChip;
+    private final Job job;
 
-	/**
-	 * @param version
-	 *            The version of the machine connected to.
-	 * @param connections
-	 *            The proxied connections we will use.
-	 * @param hostToChip
-	 *            The mapping from addresses to chip locations, to enable
-	 *            manufacturing of proxied {@link EIEIOConnection}s.
-	 * @param websocket
-	 *            The proxy handle.
-	 * @throws SpinnmanException
-	 * @throws IOException
-	 *             If we couldn't finish setting up our networking.
-	 * @throws InterruptedException
-	 *             If communications are interrupted.
-	 * @throws SpinnmanExcception
-	 *             If SpiNNaker rejects a message.
-	 */
-	@MustBeClosed
-	ProxiedTransceiver(MachineVersion version,
-			Collection<Connection> connections,
-			Map<Inet4Address, ChipLocation> hostToChip,
-			ProxyProtocolClient websocket)
-			throws IOException, SpinnmanException, InterruptedException {
-		// Assume unwrapped
-		super(version, connections);
-		this.hostToChip = hostToChip;
-		this.websocket = websocket;
-	}
+    private final ProxyProtocolClient websocket;
 
-	/** {@inheritDoc} */
-	@Override
-	public void close() throws IOException {
-		super.close();
-		websocket.close();
-	}
+    private final Map<InetAddress, ChipLocation> hostToChip = new HashMap<>();
 
-	@Override
-	public SCPConnection createScpConnection(ChipLocation chip,
-			InetAddress addr) throws IOException {
-		try {
-			return new ProxiedSCPConnection(chip, websocket, addr);
-		} catch (InterruptedException e) {
-			throw new IOException("failed to proxy connection", e);
-		}
-	}
+    /**
+     * @param version
+     *            The version of the machine connected to.
+     * @param connections
+     *            The proxied connections we will use.
+     * @param hostToChip
+     *            The mapping from addresses to chip locations, to enable
+     *            manufacturing of proxied {@link EIEIOConnection}s.
+     * @param websocket
+     *            The proxy handle.
+     * @throws SpinnmanException
+     * @throws IOException
+     *             If we couldn't finish setting up our networking.
+     * @throws InterruptedException
+     *             If communications are interrupted.
+     * @throws SpinnmanExcception
+     *             If SpiNNaker rejects a message.
+     */
+    @MustBeClosed
+    ProxiedTransceiver(Job job,
+            ProxyProtocolClient websocket)
+            throws IOException, SpinnmanException, InterruptedException {
+        super(getVersion(job), getConnections(job, websocket));
+        this.job = job;
+        this.websocket = websocket;
 
-	@Override
-	protected EIEIOConnection newEieioConnection(InetAddress localHost,
-			Integer localPort) throws IOException {
-		try {
-			return new ProxiedEIEIOListenerConnection(hostToChip, websocket);
-		} catch (InterruptedException e) {
-			throw new IOException("failed to proxy connection", e);
-		}
-	}
+        for (Connection conn : getConnections()) {
+            if (conn instanceof ProxiedSCPConnection) {
+                ProxiedSCPConnection pConn = (ProxiedSCPConnection) conn;
+                hostToChip.put(pConn.getRemoteIPAddress(), pConn.getChip());
+            }
+        }
+    }
+
+    private static MachineVersion getVersion(Job job) throws IOException {
+        var machine = job.machine();
+        return MachineVersion.bySize(machine.getWidth(), machine.getHeight());
+    }
+
+    private static List<Connection> getConnections(Job job,
+            ProxyProtocolClient ws) throws IOException, InterruptedException {
+        var conns = new ArrayList<Connection>();
+        var machine = job.machine();
+        InetAddress bootChipAddress = null;
+        for (var bc : machine.getConnections()) {
+            var chipAddr = getByNameQuietly(bc.getHostname());
+            var chipLoc = bc.getChip().asChipLocation();
+            conns.add(new ProxiedSCPConnection(chipLoc, ws, chipAddr));
+            if (chipLoc.equals(ZERO_ZERO)) {
+                bootChipAddress = chipAddr;
+            }
+        }
+        if (bootChipAddress != null) {
+            conns.add(new ProxiedBootConnection(ws, bootChipAddress));
+        }
+        return conns;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void close() throws IOException {
+        super.close();
+        websocket.close();
+    }
+
+    @Override
+    public SCPConnection createScpConnection(ChipLocation chip,
+            InetAddress addr) throws IOException {
+        try {
+            return new ProxiedSCPConnection(chip, websocket, addr);
+        } catch (InterruptedException e) {
+            throw new IOException("failed to proxy connection", e);
+        }
+    }
+
+    @Override
+    protected EIEIOConnection newEieioConnection(InetAddress localHost,
+            Integer localPort) throws IOException {
+        try {
+            return new ProxiedEIEIOListenerConnection(hostToChip, websocket);
+        } catch (InterruptedException e) {
+            throw new IOException("failed to proxy connection", e);
+        }
+    }
+
+
+    @Override
+    @ParallelSafe
+    public void writeMemory(HasCoreLocation core, MemoryLocation baseAddress,
+            InputStream dataStream, int numBytes)
+            throws IOException, ProcessException, InterruptedException {
+        ByteBuffer data = ByteBuffer.allocate(numBytes);
+        byte[] buffer = new byte[1024];
+        int remaining = numBytes;
+        while (remaining > 0) {
+            int toRead = Math.min(remaining, buffer.length);
+            int read = dataStream.read(buffer, 0, toRead);
+            if (read < 0) {
+                throw new EOFException();
+            }
+            data.put(buffer, 0, read);
+            remaining -= read;
+        }
+        try {
+            this.job.writeMemory(core, baseAddress, data);
+        } catch (SpallocServerException e) {
+            throw new IOException(e);
+        }
+    }
+
+    @Override
+    @ParallelSafe
+    public void writeMemory(HasCoreLocation core, MemoryLocation baseAddress,
+            File dataFile)
+            throws IOException, ProcessException, InterruptedException {
+        try (var stream = new FileInputStream(dataFile)) {
+            writeMemory(core, baseAddress, stream, (int) dataFile.length());
+        }
+    }
+
+    @Override
+    @ParallelSafe
+    public void writeMemory(HasCoreLocation core, MemoryLocation baseAddress,
+            ByteBuffer data)
+            throws IOException, ProcessException, InterruptedException {
+        try {
+            this.job.writeMemory(core, baseAddress, data);
+        } catch (SpallocServerException e) {
+            throw new IOException(e);
+        }
+    }
+
+    @Override
+    @CheckReturnValue
+    @ParallelSafe
+    public ByteBuffer readMemory(HasCoreLocation core,
+            MemoryLocation baseAddress, int length)
+            throws IOException, ProcessException, InterruptedException {
+        try {
+            return job.readMemory(core, baseAddress, length);
+        } catch (SpallocServerException e) {
+            throw new IOException(e);
+        }
+    }
+
 }
