b05a0e68b8febf690887b9e17c992939b13fc544
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2025-02-25 11:28:55 +0000

    Just go via proxy for fast data; don't try to go around

--- SpiNNaker-comms/src/main/java/uk/ac/manchester/spinnaker/alloc/client/ProxiedTransceiver.java
+++ SpiNNaker-comms/src/main/java/uk/ac/manchester/spinnaker/alloc/client/ProxiedTransceiver.java
@@ -56,191 +56,187 @@ import uk.ac.manchester.spinnaker.transceiver.Transceiver;
 /** A transceiver that routes messages across the proxy. */
 final class ProxiedTransceiver extends Transceiver {
 
-    private final Job job;
+	private final Job job;
 
-    private final ProxyProtocolClient websocket;
+	private final ProxyProtocolClient websocket;
 
-    private final Map<InetAddress, ChipLocation> hostToChip = new HashMap<>();
+	private final Map<InetAddress, ChipLocation> hostToChip = new HashMap<>();
 
-    /**
-     * @param version
-     *            The version of the machine connected to.
-     * @param connections
-     *            The proxied connections we will use.
-     * @param hostToChip
-     *            The mapping from addresses to chip locations, to enable
-     *            manufacturing of proxied {@link EIEIOConnection}s.
-     * @param websocket
-     *            The proxy handle.
-     * @throws SpinnmanException
-     * @throws IOException
-     *             If we couldn't finish setting up our networking.
-     * @throws InterruptedException
-     *             If communications are interrupted.
-     * @throws SpinnmanExcception
-     *             If SpiNNaker rejects a message.
-     */
-    @MustBeClosed
-    ProxiedTransceiver(Job job,
-            ProxyProtocolClient websocket)
-            throws IOException, SpinnmanException, InterruptedException {
-        super(getVersion(job), getConnections(job, websocket));
-        this.job = job;
-        this.websocket = websocket;
+	/**
+	 * @param version
+	 *            The version of the machine connected to.
+	 * @param connections
+	 *            The proxied connections we will use.
+	 * @param hostToChip
+	 *            The mapping from addresses to chip locations, to enable
+	 *            manufacturing of proxied {@link EIEIOConnection}s.
+	 * @param websocket
+	 *            The proxy handle.
+	 * @throws SpinnmanException
+	 * @throws IOException
+	 *             If we couldn't finish setting up our networking.
+	 * @throws InterruptedException
+	 *             If communications are interrupted.
+	 * @throws SpinnmanExcception
+	 *             If SpiNNaker rejects a message.
+	 */
+	@MustBeClosed
+	ProxiedTransceiver(Job job,
+			ProxyProtocolClient websocket)
+			throws IOException, SpinnmanException, InterruptedException {
+		super(getVersion(job), getConnections(job, websocket));
+		this.job = job;
+		this.websocket = websocket;
 
-        for (Connection conn : getConnections()) {
-            if (conn instanceof ProxiedSCPConnection) {
-                ProxiedSCPConnection pConn = (ProxiedSCPConnection) conn;
-                hostToChip.put(pConn.getRemoteIPAddress(), pConn.getChip());
-            }
-        }
-    }
+		for (Connection conn : getConnections()) {
+			if (conn instanceof ProxiedSCPConnection) {
+				ProxiedSCPConnection pConn = (ProxiedSCPConnection) conn;
+				hostToChip.put(pConn.getRemoteIPAddress(), pConn.getChip());
+			}
+		}
+	}
 
-    private static MachineVersion getVersion(Job job) throws IOException {
-        var machine = job.machine();
-        return MachineVersion.bySize(machine.getWidth(), machine.getHeight());
-    }
+	private static MachineVersion getVersion(Job job) throws IOException {
+		var machine = job.machine();
+		return MachineVersion.bySize(machine.getWidth(), machine.getHeight());
+	}
 
-    private static List<Connection> getConnections(Job job,
-            ProxyProtocolClient ws) throws IOException, InterruptedException {
-        var conns = new ArrayList<Connection>();
-        var machine = job.machine();
-        InetAddress bootChipAddress = null;
-        for (var bc : machine.getConnections()) {
-            var chipAddr = getByNameQuietly(bc.getHostname());
-            var chipLoc = bc.getChip().asChipLocation();
-            conns.add(new ProxiedSCPConnection(chipLoc, ws, chipAddr));
-            if (chipLoc.equals(ZERO_ZERO)) {
-                bootChipAddress = chipAddr;
-            }
-        }
-        if (bootChipAddress != null) {
-            conns.add(new ProxiedBootConnection(ws, bootChipAddress));
-        }
-        return conns;
-    }
+	private static List<Connection> getConnections(Job job,
+			ProxyProtocolClient ws) throws IOException, InterruptedException {
+		var conns = new ArrayList<Connection>();
+		var machine = job.machine();
+		InetAddress bootChipAddress = null;
+		for (var bc : machine.getConnections()) {
+			var chipAddr = getByNameQuietly(bc.getHostname());
+			var chipLoc = bc.getChip().asChipLocation();
+			conns.add(new ProxiedSCPConnection(chipLoc, ws, chipAddr));
+			if (chipLoc.equals(ZERO_ZERO)) {
+				bootChipAddress = chipAddr;
+			}
+		}
+		if (bootChipAddress != null) {
+			conns.add(new ProxiedBootConnection(ws, bootChipAddress));
+		}
+		return conns;
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    public void close() throws IOException {
-        super.close();
-        websocket.close();
-    }
+	/** {@inheritDoc} */
+	@Override
+	public void close() throws IOException {
+		super.close();
+		websocket.close();
+	}
 
-    @Override
-    public SCPConnection createScpConnection(ChipLocation chip,
-            InetAddress addr) throws IOException {
-        try {
-            return new ProxiedSCPConnection(chip, websocket, addr);
-        } catch (InterruptedException e) {
-            throw new IOException("failed to proxy connection", e);
-        }
-    }
+	@Override
+	public SCPConnection createScpConnection(ChipLocation chip,
+			InetAddress addr) throws IOException {
+		try {
+			return new ProxiedSCPConnection(chip, websocket, addr);
+		} catch (InterruptedException e) {
+			throw new IOException("failed to proxy connection", e);
+		}
+	}
 
-    @Override
-    protected EIEIOConnection newEieioConnection(InetAddress localHost,
-            Integer localPort) throws IOException {
-        try {
-            return new ProxiedEIEIOListenerConnection(hostToChip, websocket);
-        } catch (InterruptedException e) {
-            throw new IOException("failed to proxy connection", e);
-        }
-    }
+	@Override
+	protected EIEIOConnection newEieioConnection(InetAddress localHost,
+			Integer localPort) throws IOException {
+		try {
+			return new ProxiedEIEIOListenerConnection(hostToChip, websocket);
+		} catch (InterruptedException e) {
+			throw new IOException("failed to proxy connection", e);
+		}
+	}
 
 
-    @Override
-    @ParallelSafe
-    public void writeMemory(HasCoreLocation core, MemoryLocation baseAddress,
-            InputStream dataStream, int numBytes)
-            throws IOException, ProcessException, InterruptedException {
-        // If this will use a single message, just use SCP
-        if (numBytes <= UDP_MESSAGE_MAX_SIZE) {
-            super.writeMemory(core, baseAddress, dataStream, numBytes);
-        } else {
-            ByteBuffer data = ByteBuffer.allocate(numBytes);
-            byte[] buffer = new byte[1024];
-            int remaining = numBytes;
-            while (remaining > 0) {
-                int toRead = Math.min(remaining, buffer.length);
-                int read = dataStream.read(buffer, 0, toRead);
-                if (read < 0) {
-                    throw new EOFException();
-                }
-                data.put(buffer, 0, read);
-                remaining -= read;
-            }
-            this.job.writeMemory(core, baseAddress, data);
-        }
-    }
+	@Override
+	@ParallelSafe
+	public void writeMemory(HasCoreLocation core, MemoryLocation baseAddress,
+			InputStream dataStream, int numBytes)
+			throws IOException, ProcessException, InterruptedException {
+		// If this will use a single message, just use SCP
+		if (numBytes <= UDP_MESSAGE_MAX_SIZE) {
+			super.writeMemory(core, baseAddress, dataStream, numBytes);
+		} else {
+			ByteBuffer data = ByteBuffer.allocate(numBytes);
+			byte[] buffer = new byte[1024];
+			int remaining = numBytes;
+			while (remaining > 0) {
+				int toRead = Math.min(remaining, buffer.length);
+				int read = dataStream.read(buffer, 0, toRead);
+				if (read < 0) {
+					throw new EOFException();
+				}
+				data.put(buffer, 0, read);
+				remaining -= read;
+			}
+			this.job.writeMemory(core, baseAddress, data);
+		}
+	}
 
-    @Override
-    @ParallelSafe
-    public void writeMemory(HasCoreLocation core, MemoryLocation baseAddress,
-            File dataFile)
-            throws IOException, ProcessException, InterruptedException {
-        // If this will use a single message, just use SCP
-        if (dataFile.length() <= UDP_MESSAGE_MAX_SIZE) {
-            super.writeMemory(core, baseAddress, dataFile);
-        } else {
-            try (var stream = new FileInputStream(dataFile)) {
-                writeMemory(core, baseAddress, stream, (int) dataFile.length());
-            }
-        }
-    }
+	@Override
+	@ParallelSafe
+	public void writeMemory(HasCoreLocation core, MemoryLocation baseAddress,
+			File dataFile)
+			throws IOException, ProcessException, InterruptedException {
+		// If this will use a single message, just use SCP
+		if (dataFile.length() <= UDP_MESSAGE_MAX_SIZE) {
+			super.writeMemory(core, baseAddress, dataFile);
+		} else {
+			try (var stream = new FileInputStream(dataFile)) {
+				writeMemory(core, baseAddress, stream, (int) dataFile.length());
+			}
+		}
+	}
 
-    @Override
-    @ParallelSafe
-    public void writeMemory(HasCoreLocation core, MemoryLocation baseAddress,
-            ByteBuffer data)
-            throws IOException, ProcessException, InterruptedException {
-        // If this will use a single message, just use SCP
-        if (data.remaining() <= UDP_MESSAGE_MAX_SIZE) {
-            super.writeMemory(core, baseAddress, data);
-        } else {
-            this.job.writeMemory(core, baseAddress, data);
-        }
-    }
+	@Override
+	@ParallelSafe
+	public void writeMemory(HasCoreLocation core, MemoryLocation baseAddress,
+			ByteBuffer data)
+			throws IOException, ProcessException, InterruptedException {
+		// If this will use a single message, just use SCP
+		if (data.remaining() <= UDP_MESSAGE_MAX_SIZE) {
+			super.writeMemory(core, baseAddress, data);
+		} else {
+			this.job.writeMemory(core, baseAddress, data);
+		}
+	}
 
-    @Override
-    @CheckReturnValue
-    @ParallelSafe
-    public ByteBuffer readMemory(HasCoreLocation core,
-            MemoryLocation baseAddress, int length)
-            throws IOException, ProcessException, InterruptedException {
-        // If this will use a single message, just use SCP
-        if (length <= UDP_MESSAGE_MAX_SIZE) {
-            return super.readMemory(core, baseAddress, length);
-        } else {
-            return job.readMemory(core, baseAddress, length);
-        }
-    }
+	@Override
+	@CheckReturnValue
+	@ParallelSafe
+	public ByteBuffer readMemory(HasCoreLocation core,
+			MemoryLocation baseAddress, int length)
+			throws IOException, ProcessException, InterruptedException {
+		// If this will use a single message, just use SCP
+		if (length <= UDP_MESSAGE_MAX_SIZE) {
+			return super.readMemory(core, baseAddress, length);
+		} else {
+			return job.readMemory(core, baseAddress, length);
+		}
+	}
 
-    @Override
-    public void readRegion(Region region, BufferManagerStorage storage)
-            throws IOException, ProcessException, StorageException,
-            InterruptedException {
-        if (region.size < UDP_MESSAGE_MAX_SIZE) {
-            super.readRegion(region, storage);
-        } else {
-            var buffer = job.readMemory(
-                    region.core, region.startAddress, region.size);
-            storage.addRecordingContents(region, buffer);
-        }
-    }
+	@Override
+	public void readRegion(Region region, BufferManagerStorage storage)
+			throws IOException, ProcessException, StorageException,
+			InterruptedException {
+		if (region.size < UDP_MESSAGE_MAX_SIZE) {
+			super.readRegion(region, storage);
+		} else {
+			var buffer = job.readMemory(
+					region.core, region.startAddress, region.size);
+			storage.addRecordingContents(region, buffer);
+		}
+	}
 
-    @Override
-    public void writeMemoryFast(CoreLocation gathererCore,
-            ChipLocation ethernetChip, String ethernetAddress,
-            IPTag iptag, HasChipLocation chip, MemoryLocation baseAddress,
-            ByteBuffer data)
-            throws IOException, ProcessException, InterruptedException {
-        if (data.remaining() <= UDP_MESSAGE_MAX_SIZE) {
-            super.writeMemoryFast(gathererCore, ethernetChip, ethernetAddress,
-                    iptag, chip, baseAddress, data);
-        }
-        job.fastWriteData(gathererCore, ethernetChip, ethernetAddress,
-                iptag, chip, baseAddress, data);
+	@Override
+	public void writeMemoryFast(CoreLocation gathererCore,
+			ChipLocation ethernetChip, String ethernetAddress,
+			IPTag iptag, HasChipLocation chip, MemoryLocation baseAddress,
+			ByteBuffer data)
+			throws IOException, ProcessException, InterruptedException {
+		job.fastWriteData(gathererCore, ethernetChip, ethernetAddress,
+				iptag, chip, baseAddress, data);
 
-    }
+	}
 
 }
