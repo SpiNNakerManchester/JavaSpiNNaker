fcb27a340e56797beef5ad9a1d19e38b50c878a4
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2025-07-08 15:25:32 +0100

    Spaces to tabs

--- SpiNNaker-nmpiserv/src/main/java/uk/ac/manchester/spinnaker/nmpi/web/WebApplicationConfig.java
+++ SpiNNaker-nmpiserv/src/main/java/uk/ac/manchester/spinnaker/nmpi/web/WebApplicationConfig.java
@@ -43,104 +43,104 @@ import org.springframework.web.filter.DelegatingFilterProxy;
  */
 @SpringBootApplication
 public class WebApplicationConfig implements WebApplicationInitializer {
-    /**
-     * The name of the <i>system property</i> that describes where to load
-     * configuration properties from.
-     */
-    public static final String LOCATION_PROPERTY =
-            "remotespinnaker.properties.location";
+	/**
+	 * The name of the <i>system property</i> that describes where to load
+	 * configuration properties from.
+	 */
+	public static final String LOCATION_PROPERTY =
+			"remotespinnaker.properties.location";
 
-    /** The name of the filter chain. */
-    private static final String FILTER_NAME = "springSecurityFilterChain";
+	/** The name of the filter chain. */
+	private static final String FILTER_NAME = "springSecurityFilterChain";
 
-    /** Whether to add the filter or not. */
-    private static final boolean ADD_FILTER = false;
+	/** Whether to add the filter or not. */
+	private static final boolean ADD_FILTER = false;
 
-    /** Whether to add the servlet or not. */
-    private static final boolean ADD_SERVLET = true;
+	/** Whether to add the servlet or not. */
+	private static final boolean ADD_SERVLET = true;
 
-    @Override
-    public void onStartup(final ServletContext container)
-            throws ServletException {
-        System.err.println("Starting...");
-        try {
-            final var properties = getPropertySource();
-            if (ADD_SERVLET | ADD_FILTER) {
-                container.addListener(getContextLoaderListener(properties));
-            }
-            if (ADD_SERVLET) {
-                addServlet(container, properties);
-            }
-            if (ADD_FILTER) {
-                addFilterChain(container);
-            }
-        } catch (final IOException e) {
-            throw new ServletException(e);
-        }
-    }
+	@Override
+	public void onStartup(final ServletContext container)
+			throws ServletException {
+		System.err.println("Starting...");
+		try {
+			final var properties = getPropertySource();
+			if (ADD_SERVLET | ADD_FILTER) {
+				container.addListener(getContextLoaderListener(properties));
+			}
+			if (ADD_SERVLET) {
+				addServlet(container, properties);
+			}
+			if (ADD_FILTER) {
+				addFilterChain(container);
+			}
+		} catch (final IOException e) {
+			throw new ServletException(e);
+		}
+	}
 
-    /**
-     * Get the context load listener.
-     *
-     * @param properties
-     *            The properties of the listener
-     * @return The listener
-     */
-    private ContextLoaderListener getContextLoaderListener(
-            final PropertySource<?> properties) {
-        final var annotationConfig =
-                new AnnotationConfigWebApplicationContext();
-        annotationConfig.getEnvironment().getPropertySources()
-                .addFirst(properties);
-        annotationConfig.register(RemoteSpinnakerBeans.class);
-        return new ContextLoaderListener(annotationConfig);
-    }
+	/**
+	 * Get the context load listener.
+	 *
+	 * @param properties
+	 *            The properties of the listener
+	 * @return The listener
+	 */
+	private ContextLoaderListener getContextLoaderListener(
+			final PropertySource<?> properties) {
+		final var annotationConfig =
+				new AnnotationConfigWebApplicationContext();
+		annotationConfig.getEnvironment().getPropertySources()
+				.addFirst(properties);
+		annotationConfig.register(RemoteSpinnakerBeans.class);
+		return new ContextLoaderListener(annotationConfig);
+	}
 
-    /**
-     * Add a servlet.
-     *
-     * @param container
-     *            The servlet context
-     * @param properties
-     *            The properties of the servlet
-     */
-    private void addServlet(final ServletContext container,
-            final PropertySource<?> properties) {
-        container.addServlet("cxf", CXFServlet.class)
-                .addMapping(properties.getProperty("cxf.path") + "/*");
-    }
+	/**
+	 * Add a servlet.
+	 *
+	 * @param container
+	 *            The servlet context
+	 * @param properties
+	 *            The properties of the servlet
+	 */
+	private void addServlet(final ServletContext container,
+			final PropertySource<?> properties) {
+		container.addServlet("cxf", CXFServlet.class)
+				.addMapping(properties.getProperty("cxf.path") + "/*");
+	}
 
-    /**
-     * Add a filter chain.
-     *
-     * @param container
-     *            The context of the chain.
-     */
-    private void addFilterChain(final ServletContext container) {
-        container.addFilter(FILTER_NAME, new DelegatingFilterProxy(FILTER_NAME))
-                .addMappingForUrlPatterns(EnumSet.of(REQUEST, ERROR, ASYNC),
-                        false, "/*");
-    }
+	/**
+	 * Add a filter chain.
+	 *
+	 * @param container
+	 *            The context of the chain.
+	 */
+	private void addFilterChain(final ServletContext container) {
+		container.addFilter(FILTER_NAME, new DelegatingFilterProxy(FILTER_NAME))
+				.addMappingForUrlPatterns(EnumSet.of(REQUEST, ERROR, ASYNC),
+						false, "/*");
+	}
 
-    /**
-     * Get the source of the properties.
-     *
-     * @return The property source
-     * @throws IOException
-     *             If something goes wrong
-     */
-    private PropertySource<?> getPropertySource() throws IOException {
-        final var source = new File(getProperty(LOCATION_PROPERTY));
-        return new ResourcePropertySource(source.toURI().toString());
-    }
+	/**
+	 * Get the source of the properties.
+	 *
+	 * @return The property source
+	 * @throws IOException
+	 *             If something goes wrong
+	 */
+	private PropertySource<?> getPropertySource() throws IOException {
+		final var source = new File(getProperty(LOCATION_PROPERTY));
+		return new ResourcePropertySource(source.toURI().toString());
+	}
 
-    /**
-     * Spring Boot entry point.
-     *
-     * @param args
-     *            Command line arguments.
-     */
-    public static void main(String[] args) {
-        SpringApplication.run(WebApplicationConfig.class, args);
-    }
+	/**
+	 * Spring Boot entry point.
+	 *
+	 * @param args
+	 *            Command line arguments.
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplicationConfig.class, args);
+	}
 }
